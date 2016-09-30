05d338f8347b97ad3db092ab780a10861592f22e
jcast <jcast@globalscript.org>
2016-09-30 03:35:30 +0000

    [hsgs-cleanup] Inline gsfmtLocation into gshere

--- src/gsi/GSI/Util.hs
+++ src/gsi/GSI/Util.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE TemplateHaskell #-}
-module GSI.Util (Pos(Pos), gshere, gsfatal, gsfmtLocation, fmtPos) where
+module GSI.Util (Pos(Pos), gshere, gsfatal, fmtPos) where
 
 import Language.Haskell.TH.Syntax (Lit(IntegerL), Loc, location, loc_filename, loc_start)
 import Language.Haskell.TH.Lib (ExpQ, appE, conE, litE, stringE, varE)
@@ -11,17 +11,16 @@ data Pos = Pos {
   deriving (Eq, Show)
 
 gshere :: ExpQ
-gshere = location >>= gsfmtLocation
+gshere = do
+    l <- location
+    let (ln, col) = loc_start l
+    conE 'Pos `appE` (stringE $ loc_filename l) `appE` (litE $ IntegerL $ toInteger $ ln)
 
 gsfatal :: ExpQ
 gsfatal = varE 'gsfatal_w `appE` gshere
 
 gsfatal_w :: Pos -> String -> a
 gsfatal_w pos msg = error $ fmtPos pos $ msg
-
-gsfmtLocation :: Loc -> ExpQ
-gsfmtLocation l = conE 'Pos `appE` (stringE $ loc_filename l) `appE` (litE $ IntegerL $ toInteger $ ln) where
-    (ln, col) = loc_start l
 
 fmtPos :: Pos -> String -> String
 fmtPos p s = filename p ++ ':' : show (line p) ++ ": " ++ s
