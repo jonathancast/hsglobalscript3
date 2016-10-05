f0d7244e757ded10afe8884893d946ad5aa30867
jcast <jcast@globalscript.org>
2016-10-05 03:36:03 +0000

    [hsgs-threads] Work on running threads

--- src/gsi/GSI/Result.hs
+++ src/gsi/GSI/Result.hs
@@ -1,17 +1,29 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# OPTIONS_GHC -fwarn-incomplete-patterns #-}
-module GSI.Result (GSError(..), GSResult(..), implementationFailure, stCode) where
+module GSI.Result (GSError(..), GSResult(..), GSException(..), implementationFailure, stCode) where
+
+import Control.Exception (Exception(..))
+
+import Data.Typeable (Typeable)
 
 import Language.Haskell.TH.Syntax (location)
 import Language.Haskell.TH.Lib (appE, conE)
 
-import GSI.Util (Pos, gshere)
+import GSI.Util (Pos, fmtPos, gshere)
 
 data GSResult a
   = GSImplementationFailure Pos String
   | GSError GSError
 
 data GSError = GSErrUnimpl Pos
+
+data GSException
+  = GSExcUndefined Pos
+  | GSExcImplementationFailure Pos String
+  deriving (Typeable, Show)
+instance Exception GSException where
+    displayException (GSExcUndefined pos) = fmtPos pos "undefined"
+    displayException (GSExcImplementationFailure pos err) = fmtPos pos err
 
 stCode :: GSResult a -> String
 stCode GSImplementationFailure{} = "GSImplementationFailure"
--- src/gsi/GSI/Thread.hs
+++ src/gsi/GSI/Thread.hs
@@ -1,24 +1,55 @@
 {-# LANGUAGE RecursiveDo, TemplateHaskell #-}
-
+{-# OPTIONS_GHC -fwarn-incomplete-patterns #-}
 module GSI.Thread (createThread, execMainThread) where
 
-import Control.Concurrent(forkIO)
+import Control.Monad (join)
 
-import GSI.Util (gsfatal)
+import Control.Concurrent (forkIO)
+import Control.Concurrent.MVar (MVar, newEmptyMVar, newMVar, modifyMVar, putMVar, readMVar)
+import Control.Exception (throw)
+
+import GSI.Util (Pos, gsfatal, gshere)
 import GSI.Value (GSValue)
+import GSI.Result (GSException(..))
 
 data Thread a = Thread {
+    state :: MVar ThreadState,
+    wait :: MVar ()
   }
+
+data ThreadState
+  = ThreadStateRunning
+  | ThreadStateUnimpl Pos String
+
+threadStateCode :: ThreadState -> String
+threadStateCode ThreadStateRunning{} = "ThreadStateRunning"
+threadStateCode ThreadStateUnimpl{} = "ThreadStateUnimpl"
 
 createThread :: GSValue a -> IO (Thread a)
 createThread v = do
     rec
-        let t = Thread{}
+        w <- newEmptyMVar
+        sv <- newMVar ThreadStateRunning
+        let t = Thread{
+            state = sv,
+            wait = w
+          }
         tid <- forkIO $ runThread t
     return t
 
 runThread :: Thread a -> IO ()
-runThread t = return ()
+runThread t = do
+    join $ state t `modifyMVar` \ st -> case st of
+        _ -> return (ThreadStateUnimpl $gshere $ "runThread (state is " ++ threadStateCode st ++ ") next", finishThread t)
+
+finishThread t = do
+    wait t `putMVar` ()
+    return ()
 
 execMainThread :: Thread a -> IO ()
-execMainThread t = $gsfatal "execMainThread next"
+execMainThread t = do
+    readMVar $ wait t
+    st <- readMVar $ state t
+    case st of
+        ThreadStateUnimpl pos err -> throw $ GSExcImplementationFailure pos err
+        _ -> $gsfatal $ "execMainThread (state is " ++ threadStateCode st ++ ") next"
