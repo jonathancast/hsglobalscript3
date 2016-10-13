c047216c2388cf67a4a14094da0818738a3a8f8e
jcast <jcast@globalscript.org>
2016-10-13 23:56:28 +0000

    [hsgs] Over-ride Haskell's ridiculous standard policy on uncaught exceptions

--- src/gsi/test-gsi.hs
+++ src/gsi/test-gsi.hs
@@ -1,8 +1,14 @@
 {-# LANGUAGE TemplateHaskell #-}
 
+import Control.Exception (SomeException, catch, displayException)
+
+import System.Exit (ExitCode(..), exitWith)
+import System.IO (hPutStrLn, stderr)
+
 import GSI.Value (gsundefined)
 import GSI.Thread (createThread, execMainThread)
 
 main = do
     t <- createThread $gsundefined -- =<< $gsapply gsrun [gsmain]
     execMainThread t
+  `catch` \ e -> hPutStrLn stderr (displayException (e :: SomeException)) >> exitWith (ExitFailure 1) -- Because Haskell is a conspiracy to avoid good error messages
