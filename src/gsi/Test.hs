3fb5b0f22c1fd2c2130d891046fd9ee7ea25d700
jcast <jcast@globalscript.org>
2016-10-17 22:22:42 +0000

    [hsgs-exprs] Create eval threads

--- src/gsi/Test.hs
+++ src/gsi/Test.hs
@@ -6,7 +6,7 @@ import Control.Exception (displayException, fromException, try)
 import Test.HUnit
 
 import GSI.Util (Pos(Pos), gsfatal, fmtPos)
-import GSI.Value (GSValue(GSUndefined))
+import GSI.Value (GSValue(GSUndefined), gsapply_w)
 import GSI.Result (GSError(..), GSResult(..), GSException(..), stCode)
 import GSI.Eval (eval)
 import GSI.Thread (createThread, execMainThread)
@@ -20,6 +20,15 @@ main = runTestTT $ TestList $ [
             GSImplementationFailure pos msg -> assertFailure $ fmtPos pos $ ": " ++ msg
             GSError (GSErrUnimpl pos) -> assertEqual "The returned error has the right location" pos (Pos file line)
             _ -> assertFailure $ "Got " ++ stCode st ++ "; expected error"
+    ,
+    TestCase $ do
+        let file = "test-file.gs"
+        let line = 1
+        st <- eval =<< gsapply_w (Pos file line) (GSUndefined (Pos file line)) []
+        case st of
+            GSImplementationFailure pos msg -> assertFailure $ fmtPos pos $ ": " ++ msg
+            GSStack -> return ()
+            _ -> assertFailure $ "Got " ++ stCode st ++ "; expected stack"
     ,
     TestCase $ do
         let file = "test-file.gs"
