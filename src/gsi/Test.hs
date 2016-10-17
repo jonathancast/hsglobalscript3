79845f2cc40a81479cf89678581ca4b00ca02678
jcast <jcast@globalscript.org>
2016-10-17 16:57:14 +0000

    [cleanup] Fix indentation

--- src/gsi/Test.hs
+++ src/gsi/Test.hs
@@ -20,21 +20,21 @@ main = runTestTT $ TestList $ [
             GSImplementationFailure pos msg -> assertFailure $ fmtPos pos $ ": " ++ msg
             GSError (GSErrUnimpl pos) -> assertEqual "The returned error has the right location" pos (Pos file line)
             _ -> assertFailure $ "Got " ++ stCode st ++ "; expected error"
-  ,
-  TestCase $ do
-      let file = "test-file.gs"
-      let line = 1
-      t <- createThread $ GSUndefined (Pos file line)
-      return ()
-  ,
-  TestCase $ do
-      let file = "test-file.gs"
-      let line = 1
-      t <- createThread $ GSUndefined (Pos file line)
-      mb <- try $ execMainThread t
-      case mb of
-          Right _ -> assertFailure "execMainThread should throw an exception when the thread's code is undefined"
-          Left e -> case fromException e of
-              Just (GSExcUndefined pos) -> assertEqual "execMainThread should and exception with the right source location" pos (Pos file line)
-              _ -> assertFailure $ "execMainThread should throw a GSExcUndefined error, but instead threw " ++ displayException e
+    ,
+    TestCase $ do
+        let file = "test-file.gs"
+        let line = 1
+        t <- createThread $ GSUndefined (Pos file line)
+        return ()
+    ,
+    TestCase $ do
+        let file = "test-file.gs"
+        let line = 1
+        t <- createThread $ GSUndefined (Pos file line)
+        mb <- try $ execMainThread t
+        case mb of
+            Right _ -> assertFailure "execMainThread should throw an exception when the thread's code is undefined"
+            Left e -> case fromException e of
+                Just (GSExcUndefined pos) -> assertEqual "execMainThread should and exception with the right source location" pos (Pos file line)
+                _ -> assertFailure $ "execMainThread should throw a GSExcUndefined error, but instead threw " ++ displayException e
   ]
