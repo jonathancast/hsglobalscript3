efa54813adc68b76fe0fb752734b503618ceda5f
jcast <jcast@globalscript.org>
2016-10-17 16:16:38 +0000

    [hsgs] Call gsrun and gsmain

--- src/gsi/test-gsi.hs
+++ src/gsi/test-gsi.hs
@@ -5,10 +5,13 @@ import Control.Exception (SomeException, catch, displayException)
 import System.Exit (ExitCode(..), exitWith)
 import System.IO (hPutStrLn, stderr)
 
-import GSI.Value (gsundefined)
+import GSI.Value (gsapply, gsundefined)
 import GSI.Thread (createThread, execMainThread)
+import GSI.Main (gsmain)
 
 main = do
-    t <- createThread $gsundefined -- =<< $gsapply gsrun [gsmain]
+    t <- createThread =<< $gsapply gsmain [gsrun]
     execMainThread t
   `catch` \ e -> hPutStrLn stderr (displayException (e :: SomeException)) >> exitWith (ExitFailure 1) -- Because Haskell is a conspiracy to avoid good error messages
+
+gsrun = $gsundefined
