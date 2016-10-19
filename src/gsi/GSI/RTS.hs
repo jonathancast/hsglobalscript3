bbd67df53f9b453ee357eb5bef35a67d09e5b4f1
jcast <jcast@globalscript.org>
2016-10-19 23:25:44 +0000

    [hsgs-rts] Add new function await, for sleeping on an Event

--- src/gsi/GSI/RTS.hs
+++ src/gsi/GSI/RTS.hs
@@ -1,11 +1,20 @@
-module GSI.RTS (Event, newEvent, wakeup) where
+module GSI.RTS (Event, newEvent, await, wakeup) where
 
-import Control.Concurrent (MVar, newMVar, modifyMVar, tryPutMVar)
+import Control.Concurrent (MVar, newMVar, newEmptyMVar, modifyMVar, takeMVar, tryPutMVar)
 
 newtype Event = Event (MVar (Maybe [MVar ()]))
 
 newEvent :: IO Event
 newEvent = Event <$> newMVar (Just [])
+
+await :: Event -> IO ()
+await (Event mv) = do
+    mb <- modifyMVar mv $ \ st -> case st of
+        Nothing -> return (Nothing, Nothing)
+        Just mvs -> do
+            mv1 <- newEmptyMVar
+            return (Just (mv1:mvs), Just mv1)
+    maybe (return ()) takeMVar mb
 
 wakeup :: Event -> IO ()
 wakeup (Event mv) = do
