27669455e376afd9656a8a18b77a0af4fe464868
jcast <jcast@globalscript.org>
2016-11-01 03:52:10 +0000

    [hsgs-exprs] Test top-level closures

--- src/gsi/Test.hs
+++ src/gsi/Test.hs
@@ -6,12 +6,14 @@ import Control.Exception (displayException, fromException, try)
 import Test.HUnit
 
 import GSI.Util (Pos(Pos), gsfatal, fmtPos)
-import GSI.Value (GSValue(GSUndefined), gsapply_w)
+import GSI.Value (GSValue(GSUndefined), gsapply_w, gstoplevelclosure_w)
 import GSI.Result (GSError(..), GSResult(..), GSException(..), stCode)
 import GSI.Eval (eval, evalSync)
+import GSI.ByteCode (GSBCO)
 import GSI.Thread (createThread, execMainThread)
 
 main = runTestTT $ TestList $ [
+    -- §section Expressions
     TestCase $ do
         let file = "test-file.gs"
         let line = 1
@@ -39,6 +41,17 @@ main = runTestTT $ TestList $ [
             GSError (GSErrUnimpl pos) -> assertEqual "The returned error has the right location" pos (Pos file line)
             _ -> assertFailure $ "Got " ++ stCode st ++ "; expected stack"
     ,
+    TestCase $ do
+        let file = "test-file.gs"
+        let line = 1
+        st <- eval $ gstoplevelclosure_w (Pos file line) $ ($gsfatal "Evalling a known closure shouldn't touch the function" :: GSValue () -> GSBCO ())
+        case st of
+            GSImplementationFailure pos msg -> assertFailure $ fmtPos pos $ ": " ++ msg
+            GSError (GSErrUnimpl pos) -> assertEqual "The returned error has the right location" pos (Pos file line)
+            GSWHNF -> return ()
+            _ -> assertFailure $ "Got " ++ stCode st ++ "; expected stack"
+    ,
+    -- §section Threads
     TestCase $ do
         let file = "test-file.gs"
         let line = 1
