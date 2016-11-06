2618f5060526022e86ce292cb8d5ecb0ba5e7649
jcast <jcast@globalscript.org>
2016-11-06 23:46:14 +0000

    [hsgs-exprs] Implement applying lambda closures

--- src/gsi/ACE.hs
+++ src/gsi/ACE.hs
@@ -1,16 +1,24 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TemplateHaskell, ScopedTypeVariables #-}
 {-# OPTIONS_GHC -fwarn-incomplete-patterns -fno-warn-overlapping-patterns #-}
 module ACE (aceApply, aceUpdate) where
 
 import Control.Concurrent (MVar, modifyMVar)
+import Control.Exception (SomeException, catch, displayException)
 
 import GSI.RTS (wakeup)
 import GSI.Value (GSValue(..), GSThunkState(..), gsimplementationFailure, gsvCode)
+import GSI.ByteCode (GSBCO(..), bcoCode)
 import GSI.Result (GSResult(..), stCode)
 import {-# SOURCE #-} GSI.Eval (evalSync)
 
 aceApply pos fn@GSError{} args = return fn
+aceApply pos0 (GSClosure pos1 bco) args = aceCall pos0 pos1 bco args
+    `catch` \ (e :: SomeException) -> return $ $gsimplementationFailure $ "aceCall threw an exception: " ++ displayException e
 aceApply pos fn args = return $ $gsimplementationFailure $ "aceApply (function = " ++ gsvCode fn ++") next"
+
+aceCall pos0 pos1 (GSBCOFun f) (arg:args) = aceCall pos0 pos1 (f arg) args
+aceCall pos0 pos1 (GSBCOFun f) [] = return $ $gsimplementationFailure $ "aceCall (function = GSBCOFun; no args) next"
+aceCall pos0 pos1 bco args = return $ $gsimplementationFailure $ "aceCall (function = " ++ bcoCode bco ++ ") next"
 
 aceUpdate mv v = do
     mbb <- modifyMVar mv $ \ s -> case s of
--- src/gsi/GSI/ByteCode.hs
+++ src/gsi/GSI/ByteCode.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE TemplateHaskell, FlexibleInstances, MultiParamTypeClasses #-}
 {-# OPTIONS_GHC -fwarn-incomplete-patterns #-}
-module GSI.ByteCode (GSBCO, ToGSBCO(..), gsbcundefined, gsbcundefined_w) where
+module GSI.ByteCode (GSBCO(..), ToGSBCO(..), gsbcundefined, gsbcundefined_w, bcoCode) where
 
 import Language.Haskell.TH.Lib (appE, varE)
 
@@ -8,12 +8,13 @@ import GSI.Util (Pos, gsfatal, gshere)
 import GSI.Value (GSValue)
 
 data GSBCO
+  = GSBCOFun (GSValue -> GSBCO)
 
 class ToGSBCO r where
     gsbco :: r -> GSBCO
 
 instance ToGSBCO r => ToGSBCO (GSValue -> r) where
-    gsbco = $gsfatal "gsbco next"
+    gsbco f = GSBCOFun (\ v -> gsbco (f v))
 
 instance ToGSBCO GSBCO where
     gsbco = id
@@ -22,3 +23,6 @@ gsbcundefined = varE 'gsbcundefined_w `appE` gshere
 
 gsbcundefined_w :: Pos -> GSBCO
 gsbcundefined_w = $gsfatal "gsbcundefined_w next"
+
+bcoCode :: GSBCO -> String
+bcoCode GSBCOFun{} = "GSBCOFun"
