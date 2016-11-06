ab3b05a2eb10fa152567894f5857898b2f26422e
jcast <jcast@globalscript.org>
2016-11-06 23:16:57 +0000

    [hsgs-exprs] Remove StApp constructor from Stack type
    The goal here is to avoid maintaining an explicit stack at first and use Haskell as much as possible, instead

--- src/gsi/ACE.hs
+++ src/gsi/ACE.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# OPTIONS_GHC -fwarn-incomplete-patterns -fno-warn-overlapping-patterns #-}
-module ACE (Stack(..), aceEnter) where
+module ACE (Stack(..), aceApply, aceUpdate, aceEnter) where
 
 import Control.Monad (forM_)
 
@@ -8,16 +8,18 @@ import Control.Concurrent (MVar, modifyMVar)
 
 import GSI.Util (gshere)
 import GSI.RTS (wakeup)
-import GSI.Value (GSValue(..), GSThunkState(..), gsvCode)
+import GSI.Value (GSValue(..), GSThunkState(..), gsimplementationFailure, gsvCode)
 import GSI.Result (GSResult(..), stCode)
 import {-# SOURCE #-} GSI.Eval (evalSync)
 
 data Stack
-  = StApp [GSValue]
-  | StUpdate (MVar GSThunkState)
+  = StUpdate (MVar GSThunkState)
 
 aceEnter pos fn@(GSError e) stack = aceThrow fn stack
 aceEnter pos fn stack = aceUnimpl_w $gshere ("aceEnter (function = " ++ gsvCode fn ++ ") next") stack
+
+aceApply pos fn@GSError{} args = return fn
+aceApply pos fn args = return $ $gsimplementationFailure $ "aceApply (function = " ++ gsvCode fn ++") next"
 
 aceUnimpl_w pos err = aceThrow (GSImplementationFailure pos err)
 
@@ -32,5 +34,4 @@ aceUpdate mv v = do
     maybe (return ()) wakeup mbb
 
 stackCode :: Stack -> String
-stackCode StApp{} = "StApp"
 stackCode StUpdate{} = "StUpdate"
--- src/gsi/GSI/Eval.hs
+++ src/gsi/GSI/Eval.hs
@@ -9,13 +9,13 @@ import GSI.RTS (newEvent, await)
 import GSI.Value (GSValue(..), GSThunkState(..), gsimplementationFailure, gsvCode, gstsCode)
 import GSI.Result (GSError(..), GSResult(..), stCode)
 
-import ACE (Stack(..), aceEnter)
+import ACE (Stack(..), aceApply, aceUpdate)
 
 eval :: MVar (GSThunkState) -> IO GSResult
 eval mv = modifyMVar mv $ \ st -> case st of
     GSApply pos fn args -> do
         e <- newEvent
-        forkIO $ aceEnter pos fn [ StApp args, StUpdate mv ]
+        forkIO $ aceApply pos fn args >>= aceUpdate mv
         return (GSTSStack e, GSStack e)
     GSTSIndirection v -> return (GSTSIndirection v, GSIndirection v)
     _ -> return (st, GSIndirection $ $gsimplementationFailure $ "eval (thunk: " ++ gstsCode st ++ ") next")
