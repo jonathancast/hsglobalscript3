89432b113e135b7a059203bc85f965cc0e44d5bd
jcast <jcast@globalscript.org>
2016-11-16 02:43:25 +0000

    [hsgs-exprs] Remove empty module GSI.Result

--- src/gsi/GSI/Result.hs
+++
@@ -1,5 +0,0 @@
-{-# LANGUAGE TemplateHaskell #-}
-{-# OPTIONS_GHC -fwarn-incomplete-patterns -fno-warn-overlapping-patterns #-}
-module GSI.Result () where
-
-import GSI.Error (GSError(..), GSException(..))
