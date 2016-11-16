992ce3e825458dfe649b9c7708b933e132ab19e8
jcast <jcast@globalscript.org>
2016-11-16 04:06:04 +0000

    [hsgs-imp] Fix gsbcbody to throw rather than returning on failure (because imperative)

--- src/gsi/GSI/ByteCode.hs
+++ src/gsi/GSI/ByteCode.hs
@@ -2,11 +2,13 @@
 {-# OPTIONS_GHC -fwarn-incomplete-patterns #-}
 module GSI.ByteCode (GSBCO(..), ToGSBCO(..), gsbcundefined, gsbcundefined_w, gsbcbody, gsbcbody_w, bcoCode) where
 
+import Control.Exception (throwIO)
+
 import Language.Haskell.TH.Lib (appE, varE)
 
 import GSI.Util (Pos, gsfatal, gshere)
 import GSI.Value (GSValue(..), gsundefined_w)
-import GSI.ThreadType (Thread)
+import GSI.ThreadType (Thread, ThreadException(..))
 
 data GSBCO
   = GSBCOFun (GSValue -> GSBCO)
@@ -46,7 +48,7 @@ instance ToGSBCO (GSBCImp GSValue) where
 gsbcbody = varE 'gsbcbody_w `appE` gshere
 
 gsbcbody_w :: Pos -> GSBCO -> GSBCImp GSValue
-gsbcbody_w pos bco = return $ GSImplementationFailure pos $ "gsbcbody_w " ++ bcoCode bco ++ " next"
+gsbcbody_w pos bco = GSBCImp $ \ t -> throwIO $ TEImplementationFailure $gshere $ "gsbcbody_w " ++ bcoCode bco ++ " next"
 
 bcoCode :: GSBCO -> String
 bcoCode GSBCOFun{} = "GSBCOFun"
