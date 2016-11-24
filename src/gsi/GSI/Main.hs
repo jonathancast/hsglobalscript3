4575d95a97100da0125f4ae228cf7a7db3ca7be3
jcast <jcast@globalscript.org>
2016-11-24 17:09:23 +0000

    [hsgs] Fill out gsmain

--- src/gsi/GSI/Main.hs
+++ src/gsi/GSI/Main.hs
@@ -1,9 +1,13 @@
 {-# LANGUAGE TemplateHaskell #-}
 module GSI.Main (gsmain) where
 
-import GSI.Value (gstoplevelclosure)
-import GSI.ByteCode (gsbcundefined, gsbcbody)
+import GSI.Value (gsundefined, gstoplevelclosure)
+import GSI.ByteCode (gsbcundefined, gsbcapply, gsbcvar, gsbcimplet, gsbcbody)
 
+-- Main function (call this to start your interpreter)
 gsmain = $gstoplevelclosure $ \ gsrun -> do
-    $gsbcbody $ $gsbcundefined
+    args <- $gsbcimplet $ $gsbcundefined
+    $gsbcbody $ $gsbcapply gsprocessargs [ $gsbcvar args ]
 
+-- Loops over arguments to process them
+gsprocessargs = $gsundefined
