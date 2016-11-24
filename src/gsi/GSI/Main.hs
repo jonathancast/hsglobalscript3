9f34e6f95eb94548bcc4520dc18a9111b2f97036
jcast <jcast@globalscript.org>
2016-11-24 23:32:38 +0000

    [hsgs] Fill out gsmain

--- src/gsi/GSI/Main.hs
+++ src/gsi/GSI/Main.hs
@@ -2,13 +2,13 @@
 module GSI.Main (gsmain) where
 
 import GSI.Value (GSBCO, gsundefined, gstoplevelclosure)
-import GSI.ByteCode (gsbcundefined, gsbclambda, gsbcapply, gsbcprim, gsbcvar, gsbcimplet, gsbcimpbody)
+import GSI.ByteCode (gsbcundefined, gsbclambda, gsbcapply, gsbcprim, gsbcvar, gsbcimplet, gsbcimpbind, gsbcimpbody)
 import GSI.CalculusPrims (gspriminsufficientcases)
 import GSI.StdLib (gsanalyze)
 
 -- Main function (call this to start your interpreter)
 gsmain = $gstoplevelclosure $ \ gsrun -> do
-    args <- $gsbcimplet $ $gsbcundefined
+    args <- $gsbcimpbind $ $gsbcundefined
     $gsbcimpbody $ $gsbcapply gsprocessargs [ $gsbcvar args ]
 
 -- Loops over arguments to process them
