1c7648a1f60b5cb5d0ec50246dbd5a024b3a51e3
jcast <jcast@globalscript.org>
2016-12-04 02:21:31 +0000

    [hsgs-env] Implement gsprimenvGetArgs

--- src/gsi/GSI/Env.hs
+++ src/gsi/GSI/Env.hs
@@ -3,13 +3,26 @@ module GSI.Env (gsenvGetArgs) where
 
 import System.IO.Unsafe (unsafePerformIO)
 
+import Component.Monad (getM)
+
 import GSI.Util (Pos)
 import GSI.Value (GSValue, GSBCO, gsclosure)
-import GSI.Thread (Thread)
+import GSI.ThreadType (ThreadDataComponent(..), component, threadTypeName)
+import GSI.Thread (Thread, withThreadData)
 import GSI.ByteCode (gsbcimpprim)
 import API (apiImplementationFailure)
 
 gsenvGetArgs = unsafePerformIO $ $gsclosure ($gsbcimpprim gsprimenvGetArgs :: GSBCO)
 
 gsprimenvGetArgs :: Pos -> Thread -> IO GSValue
-gsprimenvGetArgs pos t = $apiImplementationFailure "gsprimenvGetArgs next"
+gsprimenvGetArgs pos t = withThreadData t (\ d -> do
+    case component d of
+        Nothing -> $apiImplementationFailure $ "gsprimenvGetArgs: This thread (" ++ threadTypeName d ++ ") lacks env.args!"
+        Just l -> do
+            GSEnvArgs args <- getM l -- \<GSEnvArgs\> is the constructor of the \<GSEnvArgs\> newtype; it exists to mark the §gs{env.args} component of the state for \<component\>
+            return args
+    )
+
+newtype GSEnvArgs = GSEnvArgs GSValue
+
+instance ThreadDataComponent GSEnvArgs where
