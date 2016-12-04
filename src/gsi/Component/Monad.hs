2807a364cf89733f10d11ea6a39d20b93904be56
jcast <jcast@globalscript.org>
2016-12-04 02:17:59 +0000

    [hsgs-component] Implement getM function

--- src/gsi/Component/Monad.hs
+++ src/gsi/Component/Monad.hs
@@ -1,3 +1,6 @@
-module Component.Monad (MonadComponentImpl) where
+module Component.Monad (MonadComponentImpl, getM) where
 
 type MonadComponentImpl m b a = (a -> (b, a)) -> m b
+
+getM :: MonadComponentImpl m a a -> m a
+getM l = l $ \ x -> (x, x)
