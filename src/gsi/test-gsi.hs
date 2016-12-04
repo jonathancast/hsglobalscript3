bf657d6981307654deb5e2ca15f731e671ab667b
jcast <jcast@globalscript.org>
2016-12-04 02:29:09 +0000

    [hsgs] Start on a thread state type for test-gsi

--- src/gsi/test-gsi.hs
+++ src/gsi/test-gsi.hs
@@ -5,14 +5,22 @@ import Control.Exception (SomeException, catch, displayException)
 import System.Exit (ExitCode(..), exitWith)
 import System.IO (hPutStrLn, stderr)
 
-import GSI.Util (gshere)
+import GSI.Util (fmtPos, gshere)
 import GSI.Value (gsapply, gsundefined)
+import GSI.ThreadType (ThreadData(..))
 import GSI.Thread (createThread, execMainThread)
 import GSI.Main (gsmain)
 
 main = do
-    t <- createThread $gshere () =<< $gsapply gsmain [gsrun]
+    t <- createThread $gshere TestGSIThread =<< $gsapply gsmain [gsrun]
     execMainThread t
   `catch` \ e -> hPutStrLn stderr (displayException (e :: SomeException)) >> exitWith (ExitFailure 1) -- Because Haskell is a conspiracy to avoid good error messages
 
 gsrun = $gsundefined
+
+data TestGSIThread = TestGSIThread{
+  }
+
+instance ThreadData TestGSIThread where
+    component d = Nothing
+    threadTypeName _ = fmtPos $gshere "TestGSIThread"
