a636a00c74702f1a64f90c730aa9a47e9e6d6dd8
jcast <jcast@globalscript.org>
2016-12-07 02:38:14 +0000

    [component] Implement MonadComponentWrapper

--- src/gsi/Component/Monad.hs
+++ src/gsi/Component/Monad.hs
@@ -1,6 +1,9 @@
-module Component.Monad (MonadComponentImpl, getM) where
+{-# LANGUAGE Rank2Types #-}
+module Component.Monad (MonadComponentImpl, MonadComponentWrapper(..), getM) where
 
 type MonadComponentImpl m b a = (a -> (b, a)) -> m b
+
+data MonadComponentWrapper m a = MonadComponentWrapper (forall b. MonadComponentImpl m b a)
 
 getM :: MonadComponentImpl m a a -> m a
 getM l = l $ \ x -> (x, x)
