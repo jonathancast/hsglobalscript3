cad06e5406b064caccb7ee026455ae38ac5fa618
jcast <jcast@globalscript.org>
2016-12-08 18:48:49 +0000

    [component] Add mvarContents function

--- src/gsi/Component/Monad.hs
+++ src/gsi/Component/Monad.hs
@@ -1,5 +1,9 @@
 {-# LANGUAGE Rank2Types #-}
-module Component.Monad (MonadComponentImpl, MonadComponentWrapper(..), getM) where
+module Component.Monad (MonadComponentImpl, MonadComponentWrapper(..), getM, mvarContents) where
+
+import Control.Concurrent.MVar (MVar, modifyMVar)
+
+type MonadComponent m a = forall b. MonadComponentImpl m b a
 
 type MonadComponentImpl m b a = (a -> (b, a)) -> m b
 
@@ -7,3 +11,6 @@ data MonadComponentWrapper m a = MonadComponentWrapper (forall b. MonadComponent
 
 getM :: MonadComponentImpl m a a -> m a
 getM l = l $ \ x -> (x, x)
+
+mvarContents :: MVar a -> MonadComponent IO a
+mvarContents mv f = modifyMVar mv (\ x -> let (y, x') = f x in return (x', y))
