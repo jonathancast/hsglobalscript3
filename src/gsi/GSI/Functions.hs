41e8946449ab55ae99e83edefdefd94d23ddd960
jcast <jcast@globalscript.org>
2016-12-18 20:07:04 +0000

    [hsgs-fns] Fill out gslist

--- src/gsi/GSI/Functions.hs
+++ src/gsi/GSI/Functions.hs
@@ -4,12 +4,13 @@ module GSI.Functions (gslist, gslist_w, gsstring, gsstring_w) where
 import Language.Haskell.TH.Lib (appE, varE)
 
 import GSI.Util (Pos, gshere)
-import GSI.Value (GSValue, gsundefined)
+import GSI.Value (GSValue(..), gsundefined)
 
 gslist = varE 'gslist_w `appE` gshere
 
 gslist_w :: Pos -> [GSValue] -> GSValue
-gslist_w pos xn = $gsundefined
+gslist_w pos [] = $gsundefined
+gslist_w pos (x:xn) = GSConstr pos ":" [ x, gslist_w pos xn ]
 
 gsstring = varE 'gsstring_w `appE` gshere
 
