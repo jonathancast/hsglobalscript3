a5187ff9a4a95ad4bf33fd53d757302f4c33e267
jcast <jcast@globalscript.org>
2016-12-20 00:16:20 +0000

    [hsgs-var] Implement GSVar

--- src/gsi/GSI/Syn.hs
+++ src/gsi/GSI/Syn.hs
@@ -1,4 +1,6 @@
-module GSI.Syn (GSVar) where -- §hs{GSVar} is deliberately §emph{abstract}
+module GSI.Syn (GSVar, gsvar) where -- §hs{GSVar} is deliberately §emph{abstract}
 
-data GSVar
+data GSVar = GSVar String
 
+gsvar :: String -> GSVar
+gsvar v = GSVar v
