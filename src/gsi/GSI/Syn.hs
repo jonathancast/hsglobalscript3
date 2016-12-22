dafa4795792fc218db9efb4bd5f40eeaa79b93fb
jcast <jcast@globalscript.org>
2016-12-22 04:39:49 +0000

    [hsgs-var] Specify symbolic vars specifically, so we can format them correctly

--- src/gsi/GSI/Syn.hs
+++ src/gsi/GSI/Syn.hs
@@ -1,13 +1,22 @@
+{-# LANGUAGE TemplateHaskell #-}
 {-# OPTIONS_GHC -fwarn-incomplete-patterns -fno-warn-overlapping-patterns #-}
 module GSI.Syn (GSVar, gsvar, fmtVarAtom) where -- §hs{GSVar} is deliberately §emph{abstract}
 
-data GSVar = GSVar String
+import Data.Char (isPunctuation, isSymbol)
+
+import GSI.Util (gsfatal)
+
+data GSVar = GSVarSym String
 
 gsvar :: String -> GSVar
-gsvar v = GSVar v
+gsvar [] = $gsfatal "gsvar [] called; this is very bad"
+gsvar v@(c:_) =
+  if isPunctuation c || isSymbol c then GSVarSym v
+  else $gsfatal $ "gsvar " ++ show v ++ " next"
 
 fmtVarAtom :: GSVar -> String -> String
+fmtVarAtom (GSVarSym o) = ('(':) . (o++) . (')':)
 fmtVarAtom v = ("<Unknown var type "++) . (gsvarCode v++) . ('>':)
 
 gsvarCode :: GSVar -> String
-gsvarCode GSVar{} = "GSVar"
+gsvarCode GSVarSym{} = "GSVarSym"
