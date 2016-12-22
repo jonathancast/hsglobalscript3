e8a9fa8cb2edda3450aff9fb7e2fcb7617380c9c
jcast <jcast@globalscript.org>
2016-12-22 19:01:05 +0000

    [hsgs-ace] Remove old apply functions

--- src/gsi/ACE.hs
+++ src/gsi/ACE.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE TemplateHaskell, ScopedTypeVariables #-}
 {-# OPTIONS_GHC -fwarn-incomplete-patterns -fno-warn-overlapping-patterns #-}
-module ACE (aceEnter, aceEnterBCO, aceApply) where
+module ACE (aceEnter, aceEnterBCO) where
 
 import GSI.Util (Pos)
 import GSI.Value (GSValue(..), GSBCO(..), GSStackFrame(..), GSThunkState(..), gsimplementationFailure, gsvCode, bcoCode, gsstCode)
@@ -26,20 +26,3 @@ aceEnterBCO pos bco@GSBCOImp{} [] = return $ GSClosure pos bco
 aceEnterBCO pos (GSBCOImp a) (k:st) = return $ $gsimplementationFailure $ "aceEnterBCO (GSBCOImp; cont = " ++ gsstCode k ++ ") next"
 aceEnterBCO pos bco st = return $ $gsimplementationFailure $ "aceEnterBCO (expr = " ++ bcoCode bco ++") next"
 
-aceApply :: Pos -> GSValue -> [GSValue] -> IO GSValue
-aceApply pos fn@GSError{} args = return fn
-aceApply pos fn@GSImplementationFailure{} args = return fn
-aceApply pos0 (GSClosure pos1 bco) args = aceCall pos0 pos1 bco args
-aceApply pos (GSThunk th) args = do
-    v <- evalSync th
-    aceApply pos v args
-aceApply pos fn args = return $ $gsimplementationFailure $ "aceApply (function = " ++ gsvCode fn ++") next"
-
-aceCall pos0 pos1 (GSBCOFun f) (arg:args) = aceCall pos0 pos1 (f arg) args
-aceCall pos0 pos1 (GSBCOFun f) [] = return $ GSClosure pos0 (GSBCOFun f)
-aceCall pos0 pos1 (GSBCOExpr e) args = do
-    v <- e
-    aceApply pos0 v args
-aceCall pos0 pos1 (GSBCOImp a) [] = return $ GSClosure pos0 (GSBCOImp a)
-aceCall pos0 pos1 (GSBCOImp a) args = return $ $gsimplementationFailure $ "aceCall: arguments to an imperative block statement?"
-aceCall pos0 pos1 bco args = return $ $gsimplementationFailure $ "aceCall (function = " ++ bcoCode bco ++ ") next"
