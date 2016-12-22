1d26d4483e01c1d3fe77fa49e45417340beea1ba
jcast <jcast@globalscript.org>
2016-12-22 04:50:57 +0000

    [hsgs-calculus] Start on gscase

--- src/gsi/GSI/StdLib.hs
+++ src/gsi/GSI/StdLib.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE TemplateHaskell, ScopedTypeVariables #-}
-module GSI.StdLib (gsanalyze) where
+module GSI.StdLib (gsanalyze, gscase) where
 
 import GSI.Value (GSValue, gsundefined, gstoplevelclosure)
 import GSI.ByteCode (gsbcundefined, gsbcapply, gsbcvar)
 
 gsanalyze = $gstoplevelclosure $ \ (e :: GSValue) (cs :: GSValue) -> $gsbcapply cs [ $gsbcvar e ]
+
+gscase = $gstoplevelclosure $ \ (p :: GSValue) (b :: GSValue) (e :: GSValue) (x :: GSValue) -> $gsbcundefined
