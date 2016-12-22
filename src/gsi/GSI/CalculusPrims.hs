85b5188f3b8459dceeed928c79bfaca7a80d6bf3
jcast <jcast@globalscript.org>
2016-12-22 04:06:48 +0000

    [hsgs-calculus] Implement gspriminsufficientcases/value

--- src/gsi/GSI/CalculusPrims.hs
+++ src/gsi/GSI/CalculusPrims.hs
@@ -2,6 +2,7 @@
 module GSI.CalculusPrims (gspriminsufficientcases) where
 
 import GSI.Util (Pos)
+import GSI.Error (GSError(..))
 import GSI.Value (GSValue(..), gsimplementationFailure, gsvCode)
 import GSI.Eval (evalSync)
 
@@ -11,4 +12,8 @@ gspriminsufficientcases pos v@GSImplementationFailure{} = return v
 gspriminsufficientcases pos (GSThunk th) = do
     v <- evalSync th
     gspriminsufficientcases pos v
+gspriminsufficientcases pos v@GSConstr{} = GSError . GSErrInsufficientCases pos . ($ "") <$> fmtValue v -- Buggy buggy should take imported-as names into account
 gspriminsufficientcases pos e = return $ $gsimplementationFailure $ "gspriminsufficientcases " ++ gsvCode e ++ " next"
+
+fmtValue :: GSValue -> IO (String -> String)
+fmtValue v = return $ ("<Unknown value: " ++) . (gsvCode v ++) . ('>':)
