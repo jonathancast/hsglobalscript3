b7891f578ff86dd2a54ced3d5cde5f36e5952a30
jcast <jcast@globalscript.org>
2016-12-22 04:32:50 +0000

    [hsgs-var] Start fmtVarAtom function

--- src/gsi/GSI/Syn.hs
+++ src/gsi/GSI/Syn.hs
@@ -1,6 +1,13 @@
-module GSI.Syn (GSVar, gsvar) where -- §hs{GSVar} is deliberately §emph{abstract}
+{-# OPTIONS_GHC -fwarn-incomplete-patterns -fno-warn-overlapping-patterns #-}
+module GSI.Syn (GSVar, gsvar, fmtVarAtom) where -- §hs{GSVar} is deliberately §emph{abstract}
 
 data GSVar = GSVar String
 
 gsvar :: String -> GSVar
 gsvar v = GSVar v
+
+fmtVarAtom :: GSVar -> String -> String
+fmtVarAtom v = ("<Unknown var type "++) . (gsvarCode v++) . ('>':)
+
+gsvarCode :: GSVar -> String
+gsvarCode GSVar{} = "GSVar"
