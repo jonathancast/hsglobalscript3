46c748b6d0cc830d52960daa33f5e33983e30df2
jcast <jcast@globalscript.org>
2016-12-26 23:58:07 +0000

    [hsgs-calculus] Fill out gscase

--- src/gsi/GSI/StdLib.hs
+++ src/gsi/GSI/StdLib.hs
@@ -7,5 +7,5 @@ import GSI.ByteCode (gsbcundefined, gsbcapply, gsbcvar, gsbcforce)
 gsanalyze = $gstoplevelclosure $ \ (e :: GSValue) (cs :: GSValue) -> $gsbcapply cs [ $gsbcvar e ]
 
 gscase = $gstoplevelclosure $ \ (p :: GSValue) (b :: GSValue) (e :: GSValue) (x :: GSValue) ->
-    $gsbcforce ($gsbcapply p [$gsbcundefined]) $ \ c -> -- Probably §hs{$gsbcvar x}
+    $gsbcforce ($gsbcapply p [$gsbcvar x]) $ \ c ->
         $gsbcundefined -- Probably §hs{$gsbcbranch ($gsbcvar e) ($gsbcvar b) c}
