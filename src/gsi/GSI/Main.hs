ed7d0ab9cd50e0d7c7d92ba9291043757e9cd335
jcast <jcast@globalscript.org>
2016-12-26 21:47:47 +0000

    [hsgs] Fill out gsprocessargs

--- src/gsi/GSI/Main.hs
+++ src/gsi/GSI/Main.hs
@@ -5,6 +5,7 @@ import GSI.Value (GSBCO, gsundefined, gstoplevelclosure)
 import GSI.ByteCode (gsbcundefined, gsbclambda, gsbcapply, gsbcprim, gsbcvar, gsbcviewpattern, gsbcimplet, gsbcimpbind, gsbcimpbody)
 import GSI.CalculusPrims (gspriminsufficientcases)
 import GSI.StdLib (gsanalyze, gscase)
+import GSI.List (gscons_view)
 import GSI.Env (gsenvGetArgs)
 
 -- Main function (call this to start your interpreter)
@@ -16,7 +17,7 @@ gsmain = $gstoplevelclosure $ \ gsrun -> do
 gsprocessargs = $gstoplevelclosure $ \ args ->
     $gsbcapply gsanalyze [ $gsbcvar args,
         $gsbcapply gscase [
-            $gsbcviewpattern $gsbcundefined $gsbcundefined $gsbcundefined, -- > 'a:'as -- Probably §hs{$gsbcvar gscons_view}, §hs{$gsbcvarpattern "a"}, and §hs{$gsbcvarpattern "as"}
+            $gsbcviewpattern ($gsbcvar gscons_view) $gsbcundefined $gsbcundefined, -- > 'a:'as -- Probably §hs{$gsbcvarpattern "a"}, and §hs{$gsbcvarpattern "as"}
             $gsbcundefined, -- Process §gs{a:as}
         $gsbclambda $ \ args -> $gsbcprim gspriminsufficientcases args :: GSBCO
         ]
