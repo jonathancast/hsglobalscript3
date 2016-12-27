e2b8fb9105d9bbecce05d90c0a813951eced38e5
jcast <jcast@globalscript.org>
2016-12-27 00:11:58 +0000

    [hsgs-calculus] Re-factor to gsbcconstr_view

--- src/gsi/GSI/List.hs
+++ src/gsi/GSI/List.hs
@@ -2,8 +2,6 @@
 module GSI.List (gscons_view) where
 
 import GSI.Value (GSValue, gsundefined, gstoplevelclosure, gsvCode)
-import GSI.ByteCode (gsbcundefined, gsbcvar, gsbcforce, gsbcimplementationfailure)
+import GSI.ByteCode (gsbcundefined, gsbcvar, gsbcforce, gsbcimplementationfailure, gsbcconstr_view)
 
-gscons_view = $gstoplevelclosure $ \ ek (sk :: GSValue) (x :: GSValue) ->
-    $gsbcforce ($gsbcvar x) $ \ x0 -> case x0 of
-        _ -> $gsbcimplementationfailure $ "gscons_view " ++ gsvCode x0 ++ " next"
+gscons_view = $gstoplevelclosure $ $gsbcconstr_view ":"
