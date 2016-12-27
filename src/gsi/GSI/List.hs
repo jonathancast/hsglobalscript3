4bcfd8124e8cb6638db3ee176d95110ca6cf30ee
jcast <jcast@globalscript.org>
2016-12-27 00:05:16 +0000

    [hsgs-calculus] Fill out gscons_view

--- src/gsi/GSI/List.hs
+++ src/gsi/GSI/List.hs
@@ -1,9 +1,9 @@
 {-# LANGUAGE TemplateHaskell, ScopedTypeVariables #-}
 module GSI.List (gscons_view) where
 
-import GSI.Value (GSValue, gsundefined, gstoplevelclosure)
-import GSI.ByteCode (gsbcundefined, gsbcvar, gsbcforce)
+import GSI.Value (GSValue, gsundefined, gstoplevelclosure, gsvCode)
+import GSI.ByteCode (gsbcundefined, gsbcvar, gsbcforce, gsbcimplementationfailure)
 
 gscons_view = $gstoplevelclosure $ \ ek (sk :: GSValue) (x :: GSValue) ->
-    $gsbcforce ($gsbcvar x) $ \ x0 ->
-        $gsbcundefined
+    $gsbcforce ($gsbcvar x) $ \ x0 -> case x0 of
+        _ -> $gsbcimplementationfailure $ "gscons_view " ++ gsvCode x0 ++ " next"
