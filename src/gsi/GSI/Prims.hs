5939fb2b3c6d0d800afbe020cdf4d4ed08e2e8ae
jcast <jcast@globalscript.org>
2016-12-28 02:47:35 +0000

    [hsgs-calculus] gsparand/stack/stack

--- src/gsi/GSI/Prims.hs
+++ src/gsi/GSI/Prims.hs
@@ -2,14 +2,22 @@
 module GSI.Prims (gsparand) where
 
 import GSI.Util (Pos)
+import GSI.RTS (awaitAny)
 import GSI.Syn (gsvar, fmtVarAtom)
 import GSI.Value (GSValue(..), gsimplementationFailure, gsvCode)
-import GSI.Eval (evalSync)
+import GSI.Eval (GSResult(..), eval, evalSync, stCode)
 
 gsparand :: Pos -> GSValue -> GSValue -> IO GSValue
 gsparand pos (GSConstr pos1 cx []) _ | cx == gsvar "0" = return $ $gsimplementationFailure $ "gsparand 0 _ next" -- > fail
 gsparand pos _ (GSConstr pos1 cy []) | cy == gsvar "0" = return $ $gsimplementationFailure $ "gsparand _ 0 next" -- > fail
-gsparand pos (GSThunk xs) (GSThunk ys) = return $ $gsimplementationFailure $ "gsparand GSThunk GSThunk next" -- eval both, wait for one, then loop
+gsparand pos x@(GSThunk xs) y@(GSThunk ys) = do
+    xr <- eval xs
+    yr <- eval ys
+    case (xr, yr) of
+        (GSStack ex, GSStack ey) -> do
+            awaitAny [ ex, ey ]
+            gsparand pos x y
+        _ -> return $ $gsimplementationFailure $ "gsparand " ++ stCode xr ++ ' ' : stCode yr ++ " next" -- eval both, wait for one, then loop
 gsparand pos (GSThunk xs) y = do
     xv <- evalSync xs
     gsparand pos xv y
