241351760cb14d791d18762276f5353eceabc11c
jcast <jcast@globalscript.org>
2016-12-28 18:09:00 +0000

    [hsgs-calculus] Fill out gscase

--- src/gsi/GSI/StdLib.hs
+++ src/gsi/GSI/StdLib.hs
@@ -1,11 +1,13 @@
 {-# LANGUAGE TemplateHaskell, ScopedTypeVariables #-}
 module GSI.StdLib (gsanalyze, gscase) where
 
-import GSI.Value (GSValue, gsundefined, gstoplevelclosure)
-import GSI.ByteCode (gsbcundefined, gsbcapply, gsbcvar, gsbcforce)
+import GSI.Syn (fmtVarAtom)
+import GSI.Value (GSValue(..), gsundefined, gstoplevelclosure, gsvCode)
+import GSI.ByteCode (gsbcundefined, gsbcapply, gsbcvar, gsbcforce, gsbcimplementationfailure)
 
 gsanalyze = $gstoplevelclosure $ \ (e :: GSValue) (cs :: GSValue) -> $gsbcapply cs [ $gsbcvar e ]
 
 gscase = $gstoplevelclosure $ \ (p :: GSValue) (b :: GSValue) (e :: GSValue) (x :: GSValue) ->
-    $gsbcforce ($gsbcapply p [$gsbcvar x]) $ \ c ->
-        $gsbcundefined -- Probably §hs{$gsbcbranch ($gsbcvar e) ($gsbcvar b) c}
+    $gsbcforce ($gsbcapply p [$gsbcvar x]) $ \ c -> case c of
+        GSConstr pos cc args -> $gsbcimplementationfailure $ "gscase (pattern returns " ++ fmtVarAtom cc ") next" -- Probably §hs{$gsbcbranch ($gsbcvar e) ($gsbcvar b) c}
+        _ -> $gsbcimplementationfailure $ "gscase (pattern returns " ++ gsvCode c ++ ") next" -- Probably §hs{$gsbcbranch ($gsbcvar e) ($gsbcvar b) c}
