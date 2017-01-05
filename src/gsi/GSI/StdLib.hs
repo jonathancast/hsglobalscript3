17f53d17e10a3c022a03d82f342da125e8885a6c
jcast <jcast@globalscript.org>
2017-01-05 01:23:30 +0000

    [hsgs-calculus] Fill out gserror

--- src/gsi/GSI/StdLib.hs
+++ src/gsi/GSI/StdLib.hs
@@ -13,4 +13,4 @@ gscase = $gslambda $ \ (p :: GSValue) (b :: GSValue) (e :: GSValue) (x :: GSValu
         GSConstr pos cc args -> $gsbcimplementationfailure $ "gscase (pattern returns " ++ fmtVarAtom cc ") next" -- Probably §hs{$gsbcbranch ($gsbcvar e) ($gsbcvar b) c}
         _ -> $gsbcimplementationfailure $ "gscase (pattern returns " ++ gsvCode c ++ ") next" -- Probably §hs{$gsbcbranch ($gsbcvar e) ($gsbcvar b) c}
 
-gserror = $gsundefined
+gserror = $gslambda $ \ (pos :: GSValue) -> $gsbcundefined
