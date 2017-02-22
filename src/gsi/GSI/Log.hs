49ee069bc593cf1a52cca438bfb51fcc4c800fdb
jcast <jcast@globalscript.org>
2017-02-22 23:06:50 +0000

    [hsgs-log] Clean up

--- src/gsi/GSI/Log.hs
+++ src/gsi/GSI/Log.hs
@@ -5,18 +5,15 @@ import Language.Haskell.TH.Lib (varE, appE)
 
 import GSI.Util (Pos, gshere)
 import GSI.Value (GSExpr, GSArg, gsae)
-import GSI.ByteCode (gsbcundefined, gsbcimplementationfailure_w, gsbcapply_w)
+import GSI.ByteCode (gsbcundefined, gsbcapply_w)
 import GSI.StdLib (gscompose)
 
 gsbclog = varE 'gsbclog_w `appE` gshere
 
 gsbclog_w :: Pos -> [GSArg] -> GSExpr
-gsbclog_w pos as = foldr
-    (\ a k -> gsbcapply_w pos gscompose [ a, $gsae k ])
-    (gsbcimplementationfailure_w $gshere $ "gsbclog_w next")
-    as
+gsbclog_w pos as = foldr (\ a k -> gsbcapply_w pos gscompose [ a, $gsae k ]) $gsbcundefined as
 
 gsbclogstring = varE 'gsbclogstring_w `appE` gshere
 
 gsbclogstring_w :: Pos -> String -> GSExpr
-gsbclogstring_w pos s = foldr (\ a k -> gsbcapply_w pos gscompose [ $gsae $gsbcundefined, $gsae $gsbcundefined ]) ($gsbcundefined) s
+gsbclogstring_w pos s = foldr (\ a k -> gsbcapply_w pos gscompose [ $gsae $gsbcundefined, $gsae $gsbcundefined ]) $gsbcundefined s
