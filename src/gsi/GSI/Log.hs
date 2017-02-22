e7241766d11244f6ca629c3698502dfc72505a98
jcast <jcast@globalscript.org>
2017-02-22 23:11:01 +0000

    [hsgs-log] New function gslogchar

--- src/gsi/GSI/Log.hs
+++ src/gsi/GSI/Log.hs
@@ -4,7 +4,7 @@ module GSI.Log (gsbclog, gsbclog_w, gsbclogstring, gsbclogstring_w) where
 import Language.Haskell.TH.Lib (varE, appE)
 
 import GSI.Util (Pos, gshere)
-import GSI.Value (GSExpr, GSArg, gsae)
+import GSI.Value (GSValue, GSExpr, GSArg, gslambda, gsae)
 import GSI.ByteCode (gsbcundefined, gsbcapply_w)
 import GSI.StdLib (gscompose)
 
@@ -12,6 +12,9 @@ gsbclog = varE 'gsbclog_w `appE` gshere
 
 gsbclog_w :: Pos -> [GSArg] -> GSExpr
 gsbclog_w pos as = foldr (\ a k -> gsbcapply_w pos gscompose [ a, $gsae k ]) $gsbcundefined as
+
+gslogchar :: GSValue
+gslogchar = $gslambda $ \ ch -> $gsbcundefined
 
 gsbclogstring = varE 'gsbclogstring_w `appE` gshere
 
