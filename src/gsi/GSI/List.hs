9be7d5eda1b08ea561ce2378dd4719dc9d970cd5
jcast <jcast@globalscript.org>
2017-03-06 07:44:44 +0000

    [hsgs-calculus] Start gscons function

--- src/gsi/GSI/List.hs
+++ src/gsi/GSI/List.hs
@@ -1,8 +1,11 @@
 {-# LANGUAGE TemplateHaskell, ScopedTypeVariables #-}
-module GSI.List (gscons_view) where
+module GSI.List (gscons, gscons_view) where
 
 import GSI.Value (GSValue, gsundefined, gslambda, gsvCode)
 import GSI.ByteCode (gsbcundefined, gsbcarg, gsbcforce, gsbcimplementationfailure, gsbcconstr_view)
+
+gscons :: GSValue
+gscons = $gsundefined
 
 gscons_view :: GSValue
 gscons_view = $gslambda $ \ ek -> $gsbcarg $ \ sk -> $gsbcarg $ \ xn -> $gsbcconstr_view ":" ek sk xn
