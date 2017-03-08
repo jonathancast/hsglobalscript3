8fa00f029a9224ece3d9bc885ba452cadfb23b2a
jcast <jcast@globalscript.org>
2017-03-08 00:54:03 +0000

    [hsgs] Fill out gsprocessargs

--- src/gsi/GSI/Main.hs
+++ src/gsi/GSI/Main.hs
@@ -1,12 +1,13 @@
 {-# LANGUAGE TemplateHaskell #-}
 module GSI.Main (gsmain) where
 
+import GSI.Syn (gsvar)
 import GSI.Value (GSExpr, gsundefined, gslambda, gsargvar, gsae)
-import GSI.ByteCode (gsbcundefined, gsbcarg, gsbcapply, gsbcprim, gsbcviewpattern, gsbcvarpattern, gsbchere, gsbcimpfor, gsbcimplet, gsbcimpbind, gsbcimpbody)
+import GSI.ByteCode (gsbcundefined, gsbcarg, gsbcapply, gsbcprim, gsbcvar, gsbcviewpattern, gsbcvarpattern, gsbchere, gsbcimpfor, gsbcimplet, gsbcimpbind, gsbcimpbody)
 import GSI.CalculusPrims (gspriminsufficientcases)
 import GSI.StdLib (gserror, gsanalyze, gscase)
 import GSI.List (gscons_view)
-import GSI.Log (gsbclog, gsbclogstring)
+import GSI.Log (gsbclog, gsbclogstring, gsloggs)
 import GSI.Env (gsenvGetArgs)
 
 -- Main function (call this to start your interpreter)
@@ -19,7 +20,7 @@ gsprocessargs = $gslambda $ \ args ->
     $gsbcapply gsanalyze [ $gsargvar args,
         $gsae ($gsbcapply gscase [ $gsae $ $gsbcviewpattern gscons_view ($gsbcvarpattern "a") ($gsbcvarpattern "as"),
             $gsae $ $gsbcarg $ \ env ->
-                $gsbcapply gserror [ $gsae $gsbchere, $gsae $ $gsbclog [ $gsae $ $gsbclogstring "Process ", $gsae $gsbcundefined, $gsae $gsbcundefined ] ], -- > log{Process }, gs gs{a}, log{ next}
+                $gsbcapply gserror [ $gsae $gsbchere, $gsae $ $gsbclog [ $gsae $ $gsbclogstring "Process ", $gsae $ $gsbcapply gsloggs [ $gsae $ $gsbcvar (gsvar "a") env ], $gsae $gsbcundefined ] ], -- > log{Process }, gs gs{a}, log{ next}
                 -- Process §gs{a:as}
         $gsae $ $gsbcarg $ \ args -> $gsbcprim gspriminsufficientcases args
         ])
