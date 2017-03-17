7b64ee4f30d79f887f377fc87f74642b724d90fc
jcast <jcast@globalscript.org>
2017-03-17 04:24:18 +0000

    [hsgs] Convert to gsav

--- src/gsi/GSI/Main.hs
+++ src/gsi/GSI/Main.hs
@@ -2,7 +2,7 @@
 module GSI.Main (gsmain) where
 
 import GSI.Syn (gsvar)
-import GSI.Value (GSExpr, gsundefined, gslambda, gsargvar, gsae)
+import GSI.Value (GSExpr, gsundefined, gslambda, gsav, gsae)
 import GSI.ByteCode (gsbcundefined, gsbcarg, gsbcapply, gsbcprim, gsbcvar, gsbcviewpattern, gsbcvarpattern, gsbchere, gsbcimpfor, gsbcimplet, gsbcimpbind, gsbcimpbody)
 import GSI.CalculusPrims (gspriminsufficientcases)
 import GSI.StdLib (gserror, gsanalyze, gscase)
@@ -12,12 +12,12 @@ import GSI.Env (gsenvGetArgs)
 
 -- Main function (call this to start your interpreter)
 gsmain = $gslambda $ \ gsrun -> $gsbcimpfor $ do
-    args <- $gsbcimpbind $ $gsargvar gsenvGetArgs
-    $gsbcimpbody $ $gsae $ $gsbcapply gsprocessargs [ $gsargvar args ]
+    args <- $gsbcimpbind $ $gsav gsenvGetArgs
+    $gsbcimpbody $ $gsae $ $gsbcapply gsprocessargs [ $gsav args ]
 
 -- Loops over arguments to process them
 gsprocessargs = $gslambda $ \ args ->
-    $gsbcapply gsanalyze [ $gsargvar args,
+    $gsbcapply gsanalyze [ $gsav args,
         $gsae ($gsbcapply gscase [ $gsae $ $gsbcviewpattern gscons_view ($gsbcvarpattern "a") ($gsbcvarpattern "as"),
             $gsae $ $gsbcarg $ \ env ->
                 $gsbcapply gserror [ $gsae $gsbchere, $gsae $ $gsbclog [ $gsae $ $gsbclogstring "Process ", $gsae $ $gsbcapply gsloggsv [ $gsae $ $gsbcvar (gsvar "a") env ], $gsae $ $gsbclogstring " next" ] ],
