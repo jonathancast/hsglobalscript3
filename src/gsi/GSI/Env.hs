e29989f7bcf3223a15af71ec6ab0e6453af9d06d
jcast <jcast@globalscript.org>
2017-03-19 03:14:19 +0000

    [hsgs-env] Fill out gsprimprintError

--- src/gsi/GSI/Env.hs
+++ src/gsi/GSI/Env.hs
@@ -3,18 +3,20 @@ module GSI.Env (GSEnvArgs(..), gsenvGetArgs, gsfileStat, gsprintError, gsENOENT_
 
 import Control.Exception (SomeException, try, fromException)
 
+import System.IO (hPutStrLn, stderr)
 import System.IO.Error (isDoesNotExistError)
 
 import Component.Monad (getM)
 
 import System.Posix.Files (getFileStatus)
 
-import GSI.Util (Pos, gshere)
+import GSI.Util (Pos, StackTrace(..), gshere, fmtPos)
 import GSI.Syn (gsvar)
-import GSI.Value (GSValue(..), gslambda, gsimpprim, gsundefined)
+import GSI.Value (GSValue(..), gslambda, gsimpprim, gsundefined, gsvCode)
 import GSI.ByteCode (gsbcarg, gsbcconstr_view)
 import GSI.ThreadType (ThreadDataComponent(..), component, threadTypeName)
 import GSI.Thread (Thread, withThreadData)
+import GSI.Eval (evalSync)
 import API (apiImplementationFailure)
 import GSI.Functions (gsapiEvalString)
 
@@ -47,7 +49,17 @@ gsprimfileStat pos t fn = do
         Right st -> $apiImplementationFailure $ "gsprimenvFileStat " ++ show fns ++ " (stat returned Right) next"
 
 gsprintError :: GSValue
-gsprintError = $gsundefined
+gsprintError = $gsimpprim gsprimprintError
+
+gsprimprintError :: Pos -> Thread -> GSValue -> IO GSValue
+gsprimprintError pos t (GSThunk th) = do
+    v <- evalSync [StackTrace $gshere [StackTrace pos []]] th
+    gsprimprintError pos t v
+gsprimprintError pos t (GSImplementationFailure pos1 msg) = do
+    hPutStrLn stderr $ fmtPos pos1 $ msg
+    return $ $gsundefined
+gsprimprintError pos t msg = do
+    $apiImplementationFailure $ "gsprimprintError " ++ gsvCode msg ++ " next"
 
 gsENOENT_view :: GSValue
 gsENOENT_view = $gslambda $ \ ek -> $gsbcarg $ \ sk -> $gsbcarg $ \ err -> $gsbcconstr_view "ENOENT" ek sk err
