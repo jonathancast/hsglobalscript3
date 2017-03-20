7050887e8aaec02aafe1b67645bd2568f86e09bc
jcast <jcast@globalscript.org>
2017-03-20 03:54:27 +0000

    [hsgs-bc] Fill out gsbcstring

--- src/gsi/GSI/String.hs
+++ src/gsi/GSI/String.hs
@@ -5,15 +5,16 @@ import Language.Haskell.TH.Lib (appE, varE)
 
 import GSI.Util (Pos, gshere)
 import GSI.Syn (gsvar, fmtVarAtom)
-import GSI.Value (GSValue(..), GSArg, GSExpr, gsav, gsvCode)
-import GSI.ByteCode (gsbcconstr, gsbcforce, gsbcimplementationfailure)
+import GSI.Value (GSValue(..), GSArg, GSExpr, gsae, gsav, gsvCode)
+import GSI.ByteCode (gsbcapply, gsbcconstr, gsbcforce, gsbcimplementationfailure)
+import GSI.List (gsappend)
 
 gsbcstring = varE 'gsbcstring_w `appE` gshere
 
 gsbcstring_w :: Pos -> [GSArg] -> GSExpr
 gsbcstring_w pos cs = gsbcstring_ww id cs where
     gsbcstring_ww :: (GSExpr -> GSExpr) -> [GSArg] -> GSExpr
-    gsbcstring_ww ds (c:cs) = $gsbcimplementationfailure "gsbcstring_w next"
+    gsbcstring_ww ds (c:cs) = gsbcstring_ww (ds . \ s1 -> $gsbcapply gsappend [ c, $gsae s1 ]) cs
     gsbcstring_ww ds [] = ds $ $gsbcconstr (gsvar "nil") []
 
 gsbcevalstring = varE 'gsbcevalstring_w `appE` gshere
