c07ce79b8ffd47be3efc2b36a48440aff6ee8af5
jcast <jcast@globalscript.org>
2017-03-21 17:45:37 +0000

    [hsgs-list] Fill out gsappend

--- src/gsi/GSI/List.hs
+++ src/gsi/GSI/List.hs
@@ -2,11 +2,19 @@
 module GSI.List (gsappend, gscons, gscons_view) where
 
 import GSI.Syn (gsvar)
-import GSI.Value (GSValue, gsundefined, gslambda, gsav, gsvCode)
-import GSI.ByteCode (gsbcundefined, gsbcarg, gsbcforce, gsbcimplementationfailure, gsbcconstr, gsbcconstr_view)
+import GSI.Value (GSValue, gsundefined, gslambda, gsae, gsav, gsvCode)
+import GSI.ByteCode (gsbcundefined, gsbcarg, gsbcapply, gsbcprim, gsbcfield, gsbcforce, gsbcimplementationfailure, gsbcconstr, gsbcconstr_view, gsbcviewpattern, gsbcvarpattern)
+import GSI.CalculusPrims (gspriminsufficientcases)
+import GSI.StdLib (gsanalyze, gscase)
 
 gsappend :: GSValue
-gsappend = $gslambda $ \ xn -> $gsbcarg $ \ ys -> $gsbcundefined
+gsappend = $gslambda $ \ xn -> $gsbcarg $ \ ys -> $gsbcapply gsanalyze [ $gsav xn,
+    $gsae $ $gsbcapply gscase [ $gsae $ $gsbcviewpattern gscons_view ($gsbcvarpattern "x") ($gsbcvarpattern "xn1"),
+        $gsae $ $gsbcarg $ \ env -> $gsbcfield (gsvar "x") env $ \ x -> $gsbcfield (gsvar "xn1") env $ \ xn1 ->
+            $gsbcapply gscons [ $gsav x, $gsae $ $gsbcapply gsappend [ $gsav xn1, $gsav ys ] ]
+    ,
+    $gsae $ $gsbcarg $ \ args -> $gsbcprim gspriminsufficientcases args
+  ]]
 
 gscons :: GSValue
 gscons = $gslambda $ \ x -> $gsbcarg $ \ xn -> $gsbcconstr (gsvar ":") [ $gsav x, $gsav xn ]
