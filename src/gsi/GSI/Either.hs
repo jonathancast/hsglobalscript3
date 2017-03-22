bef3e65d5d102bd5a6a423c4555ba4dfb74061c2
jcast <jcast@globalscript.org>
2017-03-22 18:00:35 +0000

    [hsgs-either] Add right view

--- src/gsi/GSI/Either.hs
+++ src/gsi/GSI/Either.hs
@@ -1,8 +1,11 @@
 {-# LANGUAGE TemplateHaskell #-}
-module GSI.Either (gsleft_view) where
+module GSI.Either (gsleft_view, gsright_view) where
 
 import GSI.Value (GSValue, gslambda)
 import GSI.ByteCode (gsbcarg, gsbcconstr_view)
 
 gsleft_view :: GSValue
 gsleft_view = $gslambda $ \ ek -> $gsbcarg $ \ sk -> $gsbcarg $ \ e -> $gsbcconstr_view "left" ek sk e
+
+gsright_view :: GSValue
+gsright_view = $gslambda $ \ ek -> $gsbcarg $ \ sk -> $gsbcarg $ \ e -> $gsbcconstr_view "right" ek sk e
