d88c998ebe4126c3152c19a30a200b70d9642506
jcast <jcast@globalscript.org>
2017-03-24 19:14:28 +0000

    [hsgs] Fill out matching

--- src/hsgs2hs/hsgs2hs.hs
+++ src/hsgs2hs/hsgs2hs.hs
@@ -152,7 +152,7 @@ notFollowedBy p = Parser (\ k -> k () `difference_w` runParser p ($gsfatal "retu
     p0 `difference_w` p1 = $gsfatal $ pCode p0 ++ " `difference_w` " ++ pCode p1 ++ " next"
 
 matching :: String -> (s -> Bool) -> Parser s s
-matching cat p = $gsfatal "matching next"
+matching cat p = Parser (\ k -> SymbolOrEof ($gsfatal "matching next") ($gsfatal "matching next"))
 
 pCode :: PrimParser s a -> String
 pCode PPEmpty{} = "PPEmpty"
