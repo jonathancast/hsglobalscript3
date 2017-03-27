8f8cff8e2b9882a8652eab30710f50c2d96ff539
jcast <jcast@globalscript.org>
2017-03-27 15:53:55 +0000

    [hsgs] Fill out formatExpr

--- src/hsgs2hs/hsgs2hs.hs
+++ src/hsgs2hs/hsgs2hs.hs
@@ -53,6 +53,7 @@ formatOutput (dc:dcs) = $gsfatal $ "formatOutput " ++ dcCode dc ++ " next"
 formatOutput [] = return []
 
 formatExpr :: HSExpr -> String -> String
+formatExpr (HSApp ef ex) = formatExpr ef . (' ':) . formatExprAtom ex
 formatExpr e = $gsfatal $ "formatExpr " ++ hsCode e ++ " next"
 
 formatExprAtom :: HSExpr -> String -> String
