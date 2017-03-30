a961d7c32a25ce30a59a7b021cacb83299217d52
jcast <jcast@globalscript.org>
2017-03-30 18:02:11 +0000

    [hsgs-parser] Add test for parse/empty

--- src/hsgs2hs/Test.hs
+++ src/hsgs2hs/Test.hs
@@ -1,4 +1,15 @@
 import Test.HUnit
 
-main = runTestTT $ TestList $ [
-  ]
+import Control.Applicative (Alternative(..))
+
+import System.Exit (ExitCode(..), exitWith)
+
+import GSI.Util (Pos(..))
+
+import HSGS.Parser (parse)
+
+main = do
+    c <- runTestTT $ TestList $ [
+        TestCase $ assertEqual "parse/empty" (parse (empty *> return ()) (Pos "./foo" 1 1) "") (Left "./foo:1:1: parse error")
+      ]
+    if errors c == 0 && failures c == 0 then exitWith ExitSuccess else exitWith (ExitFailure 1)
