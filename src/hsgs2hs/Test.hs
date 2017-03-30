f5d779d709682dc37f6d3f2b1a1085aea3304fb0
jcast <jcast@globalscript.org>
2017-03-30 18:19:35 +0000

    [hsgs-parser] Clean up tests

--- src/hsgs2hs/Test.hs
+++ src/hsgs2hs/Test.hs
@@ -6,11 +6,18 @@ import System.Exit (ExitCode(..), exitWith)
 
 import GSI.Util (Pos(..))
 
-import HSGS.Parser (parse, pfail)
+import HSGS.Parser (Parser, parse, pfail)
 
 main = do
+    let pos = Pos "./foo" 1 1
     c <- runTestTT $ TestList $ [
-        TestCase $ assertEqual "parse/empty" (parse (empty *> return ()) (Pos "./foo" 1 1) "") (Left "./foo:1:1: Unexpected \"\""),
-        TestCase $ assertEqual "parse/empty" (parse (pfail "bleargh" *> return ()) (Pos "./foo" 1 1) "") (Left "./foo:1:1: Unexpected \"\"; bleargh")
+        let
+            p :: Parser Char ()
+            p = empty
+        in TestCase $ assertEqual "parse/empty" (parse p pos "") (Left "./foo:1:1: Unexpected \"\""),
+        let
+            p :: Parser Char ()
+            p = pfail "bleargh"
+        in TestCase $ assertEqual "parse/empty" (parse p pos "") (Left "./foo:1:1: Unexpected \"\"; bleargh")
       ]
     if errors c == 0 && failures c == 0 then exitWith ExitSuccess else exitWith (ExitFailure 1)
