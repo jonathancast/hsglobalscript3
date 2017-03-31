8905a572d2024ef2eda795b52a9ab685cb3ffa8a
jcast <jcast@globalscript.org>
2017-03-31 17:43:41 +0000

    [hsgs] Fill out compileArg

--- src/hsgs2hs/hsgs2hs.hs
+++ src/hsgs2hs/hsgs2hs.hs
@@ -125,6 +125,7 @@ compileArg env (EVar _ v) = return (
     HSConstr "GSArgVar" `HSApp` HSVar v
   )
 compileArg env (EPat pos p) = compilePatArg env pos p
+compileArg env (EOpen pos e) = compileOpenArg env pos e
 compileArg env e = $gsfatal $ "compileArg " ++ eCode e ++ " next"
 
 compileExpr :: Env -> Expr -> Either String (Set HSImport, HSExpr)
