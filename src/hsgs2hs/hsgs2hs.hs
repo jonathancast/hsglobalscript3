6884f38dfad1cb29bfb28f1e9d3e6040a1cd8ca3
jcast <jcast@globalscript.org>
2017-04-09 22:33:21 +0000

    [hsgs] Add view (:) to global environment

--- src/hsgs2hs/hsgs2hs.hs
+++ src/hsgs2hs/hsgs2hs.hs
@@ -409,7 +409,8 @@ globalEnv = Env{
     gsviews = Map.fromList [
         ("ENOENT", (Set.singleton $ HSIVar "GSI.Env" "gsENOENT_view", HSVar "gsENOENT_view")),
         ("left", (Set.singleton $ HSIVar "GSI.Either" "gsleft_view", HSVar "gsleft_view")),
-        ("right", (Set.singleton $ HSIVar "GSI.Either" "gsright_view", HSVar "gsright_view"))
+        ("right", (Set.singleton $ HSIVar "GSI.Either" "gsright_view", HSVar "gsright_view")),
+        (":", (Set.singleton $ HSIVar "GSI.List" "gscons_view", HSVar "gscons_view"))
     ],
     gssignatures = Map.fromList [
         ("case", \ as -> case as of
