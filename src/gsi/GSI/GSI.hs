a6b938670d873dff6d89f5dea65811803648baea
jcast <jcast@globalscript.org>
2017-04-17 22:12:20 +0000

    [hsgs] Fill out gsicreateThread

--- src/gsi/GSI/GSI.hs
+++ src/gsi/GSI/GSI.hs
@@ -1,8 +1,16 @@
 {-# LANGUAGE TemplateHaskell #-}
 module GSI.GSI (gsigsundefined, gsicreateThread) where
 
-import GSI.Value (gsundefined_value)
+import GSI.Util (Pos)
+import GSI.Value (GSValue, gsimpprim, gsundefined_value)
+import GSI.ThreadType (Thread)
+import API (apiImplementationFailure)
 
 gsigsundefined = $gsundefined_value
 
-gsicreateThread = $gsundefined_value
+gsicreateThread :: GSValue
+gsicreateThread = $gsimpprim gsiprimcreateThread
+
+gsiprimcreateThread :: Pos -> Thread -> GSValue -> GSValue -> IO GSValue
+gsiprimcreateThread pos t td v = do
+    $apiImplementationFailure $ "gsiprimcreateThread next"
