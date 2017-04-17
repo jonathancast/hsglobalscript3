257846480fd2fd048fd7ee19e46b789a2f31e96b
jcast <jcast@globalscript.org>
2017-04-17 21:29:28 +0000

    [hsgs] Add function gsicreateThread

--- src/gsi/GSI/GSI.hs
+++ src/gsi/GSI/GSI.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE TemplateHaskell #-}
-module GSI.GSI (gsigsundefined) where
+module GSI.GSI (gsigsundefined, gsicreateThread) where
 
 import GSI.Value (gsundefined_value)
 
 gsigsundefined = $gsundefined_value
+
+gsicreateThread = $gsundefined_value
