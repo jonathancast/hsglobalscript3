3d6774fa584381aa7c2c3f13bfd58e74ea17fdbe
jcast <jcast@globalscript.org>
2017-04-17 21:29:41 +0000

    [hsgs] Fill out gsi

--- src/gsi/gsi.hs
+++ src/gsi/gsi.hs
@@ -14,8 +14,9 @@ import GSI.Value (gslambda, gsapply, gsundefined_value, gsae)
 import GSI.ThreadType (ThreadData(..), fetchThreadDataComponent, insertThreadDataComponent, emptyThreadDataComponents)
 import GSI.Thread (createThread, execMainThread)
 import GSI.Functions (gslist, gsstring)
-import GSI.ByteCode (gsbcarg, gsbcundefined, gsbcimpfor, gsbcimpbind, gsbcimpbody)
+import GSI.ByteCode (gsbcarg, gsbcapply, gsbcundefined, gsbcimpfor, gsbcimpbind, gsbcimpbody)
 import GSI.Env (GSEnvArgs(..))
+import GSI.GSI (gsicreateThread)
 import GSI.Main (gsmain)
 
 main = do
@@ -25,7 +26,7 @@ main = do
   `catch` \ e -> hPutStrLn stderr (displayException (e :: SomeException)) >> exitWith (ExitFailure 1) -- Because Haskell is a conspiracy to avoid good error messages
 
 gsrun = $gslambda $ \ prog -> $gsbcarg $ \ args -> $gsbcimpfor $ do
-    t <- $gsbcimpbind $ $gsae $ $gsbcundefined
+    t <- $gsbcimpbind $ $gsae $ $gsbcapply gsicreateThread [ $gsae $ $gsbcundefined, $gsae $ $gsbcundefined ] -- hs{GSIThread{ envArgs = args }, gs{$gsbcapply gsmain [prog]}
     $gsbcimpbody $ $gsae $ $gsbcundefined
 
 data GSIThread = GSIThread{
