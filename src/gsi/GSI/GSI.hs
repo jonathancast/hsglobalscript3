4ba2096b045b0e984922de3829e9c7bbe81a5a11
jcast <jcast@globalscript.org>
2017-04-18 15:43:09 +0000

    [hsgs] Add type GSIThreadData

--- src/gsi/GSI/GSI.hs
+++ src/gsi/GSI/GSI.hs
@@ -1,9 +1,9 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TemplateHaskell, ExistentialQuantification #-}
 module GSI.GSI (gsigsundefined, gsicreateThread) where
 
 import GSI.Util (Pos)
-import GSI.Value (GSValue, gsimpprim, gsundefined_value)
-import GSI.ThreadType (Thread)
+import GSI.Value (GSValue, GSExternal, gsimpprim, gsundefined_value)
+import GSI.ThreadType (Thread, ThreadData)
 import API (apiImplementationFailure)
 
 gsigsundefined = $gsundefined_value
@@ -14,3 +14,7 @@ gsicreateThread = $gsimpprim gsiprimcreateThread
 gsiprimcreateThread :: Pos -> Thread -> GSValue -> GSValue -> IO GSValue
 gsiprimcreateThread pos t td v = do
     $apiImplementationFailure $ "gsiprimcreateThread next"
+
+data GSIThreadData = forall d. ThreadData d => GSIThreadData d
+
+instance GSExternal GSIThreadData
