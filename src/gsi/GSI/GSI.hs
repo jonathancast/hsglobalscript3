e76ba2409c20b59ee9141dbc57b1e9c79aa0bc88
jcast <jcast@globalscript.org>
2017-04-19 01:41:50 +0000

    [hsgs] Fill out createThread

--- src/gsi/GSI/GSI.hs
+++ src/gsi/GSI/GSI.hs
@@ -8,6 +8,7 @@ import Component.Monad (mvarContents)
 import GSI.Util (Pos, fmtPos, gshere)
 import GSI.Value (GSValue(..), GSExternal(..), gslambda, gsimpprim, gsundefined_value, gsundefined_value_w, gsav)
 import GSI.ThreadType (Thread, ThreadData(..), fetchThreadDataComponent, insertThreadDataComponent, emptyThreadDataComponents)
+import GSI.Thread (createThread)
 import API (apiImplementationFailure)
 import GSI.Functions (gsapiEvalExternal, gsapiEvalList)
 import GSI.ByteCode (gsbcexternal, gsbcundefined)
@@ -33,8 +34,9 @@ gsicreateThread = $gsimpprim gsiprimcreateThread
 
 gsiprimcreateThread :: Pos -> Thread -> GSValue -> GSValue -> IO GSValue
 gsiprimcreateThread pos t tdv vv = do
-    td <- gsapiEvalExternal pos tdv :: IO GSIThreadData
-    v <- gsapiEvalExternal pos vv :: IO GSIGSValue
+    GSIThreadData td <- gsapiEvalExternal pos tdv
+    GSIGSValue v <- gsapiEvalExternal pos vv
+    t <- createThread pos td v
     $apiImplementationFailure $ "gsiprimcreateThread next"
 
 gsigsiThreadData :: GSValue
