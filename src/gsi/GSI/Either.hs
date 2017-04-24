fd05268683e807a65239100b366980ffded41da7
jcast <jcast@globalscript.org>
2017-04-24 21:17:01 +0000

    [hsgs] Fill out gseithermap

--- src/gsi/GSI/Either.hs
+++ src/gsi/GSI/Either.hs
@@ -2,9 +2,10 @@
 module GSI.Either (gseitherFor, gseitherbind, gseithermap, gsleft, gsright, gsleft_view, gsright_view) where
 
 import GSI.Syn (gsvar)
-import GSI.Value (GSValue, gslambda, gsav, gsundefined_value)
-import GSI.ByteCode (gsbcarg, gsbcapply, gsbcprim, gsbcconstr, gsbcconstr_view, gsbcundefined)
+import GSI.Value (GSValue, gslambda, gsav, gsae, gsundefined_value)
+import GSI.ByteCode (gsbcarg, gsbclfield, gsbcapply, gsbcprim, gsbcconstr, gsbcviewpattern, gsbcconstr_view, gsbcvarpattern, gsbcundefined)
 import GSI.CalculusPrims (gspriminsufficientcases)
+import GSI.StdLib (gsanalyze, gscase)
 
 gseitherFor :: GSValue
 gseitherFor = $gslambda $ \ gen -> $gsbcarg $ \ k -> $gsbcapply gseitherbind [ $gsav gen, $gsav k ]
@@ -13,7 +14,11 @@ gseitherbind :: GSValue
 gseitherbind = $gslambda $ \ e -> $gsbcarg $ \ k -> $gsbcprim gspriminsufficientcases e
 
 gseithermap :: GSValue
-gseithermap = $gslambda $ \ f -> $gsbcarg $ \ e -> $gsbcprim gspriminsufficientcases e
+gseithermap = $gslambda $ \ f -> $gsbcarg $ \ e -> $gsbcapply gsanalyze [ $gsav e,
+    $gsae $ $gsbcapply gscase [ $gsae $ $gsbcviewpattern gsright_view ($gsbcvarpattern "x"),
+        $gsae $ $gsbcarg $ \ env -> $gsbclfield (gsvar "x") env $ \ x -> $gsbcapply gsright [ $gsae $ $gsbcundefined ],
+    $gsae $ $gsbcarg $ \ e -> $gsbcprim gspriminsufficientcases e
+  ]]
 
 gsleft :: GSValue
 gsleft = $gslambda $ \ x -> $gsbcconstr (gsvar "left") [ $gsav x ]
