dbb024c6dfe08709d9b493fbd8b941e38cf4f665
jcast <jcast@globalscript.org>
2017-04-24 20:50:25 +0000

    [hsgs] Add function gseitherbind

--- src/gsi/GSI/Either.hs
+++ src/gsi/GSI/Either.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE TemplateHaskell #-}
-module GSI.Either (gseitherFor, gsleft, gsright, gsleft_view, gsright_view) where
+module GSI.Either (gseitherFor, gseitherbind, gsleft, gsright, gsleft_view, gsright_view) where
 
 import GSI.Syn (gsvar)
 import GSI.Value (GSValue, gslambda, gsav, gsundefined_value)
@@ -7,6 +7,9 @@ import GSI.ByteCode (gsbcarg, gsbcconstr, gsbcconstr_view, gsbcundefined)
 
 gseitherFor :: GSValue
 gseitherFor = $gsundefined_value
+
+gseitherbind :: GSValue
+gseitherbind = $gsundefined_value
 
 gsleft :: GSValue
 gsleft = $gslambda $ \ x -> $gsbcconstr (gsvar "left") [ $gsav x ]
