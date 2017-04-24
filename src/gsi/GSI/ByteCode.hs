79060a42ee8930a5c4c9adbc2dcd7703e97d95b8
jcast <jcast@globalscript.org>
2017-04-24 20:49:29 +0000

    [hsgs] Add function gsbcemptygen

--- src/gsi/GSI/ByteCode.hs
+++ src/gsi/GSI/ByteCode.hs
@@ -2,7 +2,7 @@
 {-# OPTIONS_GHC -fwarn-incomplete-patterns -fno-warn-overlapping-patterns #-}
 module GSI.ByteCode (
     gsbcundefined, gsbcundefined_w, gsbcarg, gsbcarg_w, gsbcenter, gsbcenter_w, gsbcapply, gsbcapply_w, gsbcprim, gsbcprim_w, gsbcimpprim, gsbcimpprim_w, gsbcforce, gsbcforce_w, gsbclfield, gsbclfield_w, gsbcfield, gsbcfield_w, gsbcrecord, gsbcrecord_w, gsbcconstr, gsbcconstr_w, gsbcexternal, gsbcexternal_w, gsbchere, gsbchere_w, gsbcerror, gsbcerror_w, gsbcimplementationfailure, gsbcimplementationfailure_w,
-    gsbccomposegen_w, gsbcexecgen_w, gsbcvarbind_w,
+    gsbccomposegen_w, gsbcexecgen_w, gsbcvarbind_w, gsbcemptygen_w,
     gsbcevalnatural, gsbcevalnatural_w, gsbcfmterrormsg, gsbcfmterrormsg_w,
     gsbcimpfor, gsbcimplet, gsbcimplet_w, gsbcimpbind, gsbcimpbind_w, gsbcimpbody, gsbcimpbody_w,
     gsbccomposeimpgen_w, gsbcimpexecbind_w, gsbcimpvarbind_w, gsbcemptyimpgen_w,
@@ -179,6 +179,9 @@ gsbcexecgen_w pos gsmap e = gsbcimplementationfailure_w $gshere $ "gsbcexecgen_w
 
 gsbcvarbind_w :: Pos -> GSArg -> GSVar -> GSArg -> GSExpr
 gsbcvarbind_w pos gsmap x e = gsbcimplementationfailure_w $gshere $ "gsbcvarbind_w next"
+
+gsbcemptygen_w :: Pos -> GSArg -> GSExpr
+gsbcemptygen_w pos gsunit = gsbcimplementationfailure_w $gshere $ "gsbcemptygen_w next"
 
 gsbcimplet = varE 'gsbcimplet_w `appE` gshere
 
