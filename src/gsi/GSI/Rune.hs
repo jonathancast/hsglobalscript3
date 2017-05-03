94d3e2c2f5d449407b41792386418c67d0706cd6
jcast <jcast@globalscript.org>
2017-05-03 14:46:37 +0000

    [hsgs] Fill out rune.≡

--- src/gsi/GSI/Rune.hs
+++ src/gsi/GSI/Rune.hs
@@ -1,7 +1,13 @@
 {-# LANGUAGE TemplateHaskell #-}
 module GSI.Rune (gsruneEq) where
 
-import GSI.Value (gslambda, gsundefined_value, gsav)
-import GSI.ByteCode (gsbcarg, gsbcforce, gsbcundefined)
+import GSI.Syn (gsvar)
+import GSI.Value (GSValue(..), gslambda, gsundefined_value, gsav, gsvCode)
+import GSI.ByteCode (gsbcarg, gsbcforce, gsbcconstr, gsbcundefined, gsbcimplementationfailure)
 
-gsruneEq = $gslambda $ \ r0 -> $gsbcarg $ \ r1 -> $gsbcforce ($gsav r0) $ \ r0v -> $gsbcundefined
+gsruneEq = $gslambda $ \ r0 -> $gsbcarg $ \ r1 -> $gsbcforce ($gsav r0) $ \ r0v -> $gsbcforce ($gsav r1) $ \ r1v ->
+    case (r0v, r1v) of
+        (GSRune c0, GSRune c1)
+            | c0 == c1 -> $gsbcconstr (gsvar "true") []
+            | otherwise -> $gsbcconstr (gsvar "false") []
+        _ -> $gsbcimplementationfailure $ "gsruneEq " ++ gsvCode r0v ++ ' ' : gsvCode r1v ++ " next"
