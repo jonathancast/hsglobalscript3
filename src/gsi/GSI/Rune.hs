2c4dae4e772248c5e53a6024b4b1b62b37300f58
jcast <jcast@globalscript.org>
2017-05-24 22:52:47 +0000

    [hsgs] Fill out is-lower

--- src/gsi/GSI/Rune.hs
+++ src/gsi/GSI/Rune.hs
@@ -1,11 +1,15 @@
 {-# LANGUAGE TemplateHaskell #-}
 module GSI.Rune (gsisLower, gsrune_neq, gsruneEq) where
 
+import Data.Char (isLower)
+
 import GSI.Syn (gsvar)
 import GSI.Value (GSValue(..), gslambda_value, gsundefined_value, gsav, gsvCode)
 import GSI.ByteCode (gsbcarg, gsbcforce, gsbcconstr, gsbcundefined, gsbcimplementationfailure)
 
-gsisLower = $gsundefined_value
+gsisLower = $gslambda_value $ \ r -> $gsbcforce ($gsav r) $ \ r0 -> case r0 of
+    GSRune r0v -> if isLower r0v then $gsbcconstr (gsvar "true") [] else $gsbcconstr (gsvar "false") []
+    _ -> $gsbcimplementationfailure $ "gsisLower " ++ gsvCode r0 ++ " next"
 
 gsrune_neq = $gslambda_value $ \ c0 -> $gsbcarg $ \ c1 -> $gsbcforce ($gsav c0) $ \ c0_0 -> $gsbcforce ($gsav c1) $ \ c1_0 -> case (c0_0, c1_0) of
     (GSRune c0hs, GSRune c1hs) ->
