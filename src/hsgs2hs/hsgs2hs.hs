bb3dbdc11349e797ceba0a6c5d3305e76f21d1a6
jcast <jcast@globalscript.org>
2017-06-02 03:48:54 +0000

    [hsgs] Remove view get-pos from the global environment

--- src/hsgs2hs/hsgs2hs.hs
+++ src/hsgs2hs/hsgs2hs.hs
@@ -647,7 +647,6 @@ globalEnv = Env{
         ("ENOENT", (Set.singleton $ HSIVar "GSI.Env" "gsENOENT_view", HSVar "gsENOENT_view")),
         ("false", (Set.singleton $ HSIVar "GSI.Bool" "gsfalse_view", HSVar "gsfalse_view")),
         ("file.document", (Set.singleton $ HSIVar "GSI.Main" "gsfileDocument_view", HSVar "gsfileDocument_view")),
-        ("get-pos", (Set.singleton $ HSIVar "GSI.Parser" "gsparser_getPos_view", HSVar "gsparser_getPos_view")),
         ("left", (Set.singleton $ HSIVar "GSI.Either" "gsleft_view", HSVar "gsleft_view")),
         ("nil", (Set.singleton $ HSIVar "GSI.List" "gsnil_view", HSVar "gsnil_view")),
         ("right", (Set.singleton $ HSIVar "GSI.Either" "gsright_view", HSVar "gsright_view")),
