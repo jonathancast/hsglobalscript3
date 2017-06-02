ab14ae9401a52e6fc50435b4b285a51f8e73db98
jcast <jcast@globalscript.org>
2017-06-02 00:50:39 +0000

    [hsgs] Fill out gsparand

--- src/gsi/GSI/CalculusPrims.hs
+++ src/gsi/GSI/CalculusPrims.hs
@@ -12,7 +12,7 @@ import GSI.Eval (GSResult(..), eval, evalSync, stCode)
 
 gsparand :: Pos -> GSValue -> GSValue -> IO GSValue
 gsparand pos (GSConstr pos1 cx []) _ | cx == gsvar "0" = return $ $gsimplementationfailure $ "gsparand 0 _ next" -- > fail
-gsparand pos _ (GSConstr pos1 cy []) | cy == gsvar "0" = return $ $gsimplementationfailure $ "gsparand _ 0 next" -- > fail
+gsparand pos _ (GSConstr pos1 cy []) | cy == gsvar "0" = return $ GSConstr pos (gsvar "0") []
 gsparand pos (GSThunk xs) (GSThunk ys) = do
     (xv, yv) <- gspareval pos xs ys
     gsparand pos xv yv
