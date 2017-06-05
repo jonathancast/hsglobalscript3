69ecb83ce4f32f0f9acc8ddb6123f67375d1a6ae
jcast <jcast@globalscript.org>
2017-06-05 01:30:03 +0000

    [hsgs] Fill out gsstrun

--- src/gsi/GSI/ST.hs
+++ src/gsi/GSI/ST.hs
@@ -1,6 +1,12 @@
 {-# LANGUAGE TemplateHaskell #-}
 module GSI.ST (gsstrun) where
 
-import GSI.Value (gsundefined_value)
+import GSI.Util (Pos)
+import GSI.Value (GSValue(..), gsprim, gsundefined_value, gsimplementationfailure, gsvCode)
 
-gsstrun = $gsundefined_value
+gsstrun :: GSValue
+gsstrun = $gsprim gsprim_st_run :: GSValue
+
+gsprim_st_run :: Pos -> GSValue -> IO GSValue
+gsprim_st_run pos a = do
+    return $ $gsimplementationfailure $ "gsprim_st_run " ++ gsvCode a ++ " next"
