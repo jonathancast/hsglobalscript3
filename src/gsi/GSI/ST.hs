42557efd2d2566f55932f0ca7f91661640b275a2
jcast <jcast@globalscript.org>
2017-06-05 02:22:49 +0000

    [hsgs] Fill out st.run

--- src/gsi/GSI/ST.hs
+++ src/gsi/GSI/ST.hs
@@ -4,7 +4,7 @@ module GSI.ST (gsstrun) where
 import GSI.Util (Pos)
 import GSI.Value (GSValue(..), gsprim, gsundefined_value, gsimplementationfailure, gsvCode)
 import GSI.ThreadType (ThreadData(..), ThreadState(..), threadStateCode)
-import GSI.Thread (createThread, waitThread, createPromise)
+import GSI.Thread (createThread, waitThread, createPromise, readPromise)
 
 gsstrun :: GSValue
 gsstrun = $gsprim gsprim_st_run :: GSValue
@@ -17,6 +17,7 @@ gsprim_st_run pos a = do
     case st of
         ThreadStateUnimpl pos err -> return $ GSImplementationFailure pos err
         ThreadStateError err -> return $ GSError err
+        ThreadStateSuccess -> readPromise pr
         _ -> return $ $gsimplementationfailure $ "st.run (state is " ++ threadStateCode st ++ ") next"
 
 stThreadData = ThreadData{
