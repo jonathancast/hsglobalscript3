8328c7774c18d593c01a90cc65e40b4a1d3a0d8b
jonathanccast <jonathanccast@fastmail.fm>
2017-11-23 00:37:45 +0000

    [hsgs] Add function parser.env.missing-body.insert

--- src/hsgs2hs/hsgs2hs.hs
+++ src/hsgs2hs/hsgs2hs.hs
@@ -759,6 +759,7 @@ globalEnv = Env{
         ("parser.env.lambda-like.insert", (Set.singleton $ HSIVar "GSDL.Parser" "gsdl_parser_env_lambda_like_insert", HSVar "gsdl_parser_env_lambda_like_insert")),
         ("parser.env.lambda-likes", (Set.singleton $ HSIVar "GSDL.Parser" "gsdl_parser_env_lambda_likes", HSVar "gsdl_parser_env_lambda_likes")),
         ("parser.env.missing-body.get", (Set.singleton $ HSIVar "GSDL.Parser" "gsdl_parser_env_missing_body_get", HSVar "gsdl_parser_env_missing_body_get")),
+        ("parser.env.missing-body.insert", (Set.singleton $ HSIVar "GSDL.Parser" "gsdl_parser_env_missing_body_insert", HSVar "gsdl_parser_env_missing_body_insert")),
         ("parser.env.signature.get", (Set.singleton $ HSIVar "GSDL.Parser" "gsdl_parser_env_signature_get", HSVar "gsdl_parser_env_signature_get")),
         ("parser.env.signature.insert", (Set.singleton $ HSIVar "GSDL.Parser" "gsdl_parser_env_signature_insert", HSVar "gsdl_parser_env_signature_insert")),
         ("parser.fail", (Set.singleton $ HSIVar "GSI.Parser" "gsparser_fail", HSVar "gsparser_fail")),
