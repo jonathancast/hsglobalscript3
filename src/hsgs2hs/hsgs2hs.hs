e2aa7fa10254d0edc5136281db6615ab5ae0e422
jonathanccast <jonathanccast@fastmail.fm>
2018-02-13 03:01:17 +0000

    [hsgs] Add function validator.env.empty

--- src/hsgs2hs/hsgs2hs.hs
+++ src/hsgs2hs/hsgs2hs.hs
@@ -837,6 +837,7 @@ globalEnv = Env{
         ("type.unifiable-var", (Set.singleton $ HSIVar "GSDL.AST" "gstype_unifiable_var", HSVar "gstype_unifiable_var")),
         ("undefined", (Set.singleton $ HSIVar "GSI.StdLib" "gsundefined", HSVar "gsundefined")),
         ("validate-document", (Set.singleton $ HSIVar "GSDL.Validate" "gsvalidate_document", HSVar "gsvalidate_document")),
+        ("validator.env.empty", (Set.singleton $ HSIVar "GSDL.Validator" "gsvalidator_env_empty", HSVar "gsvalidator_env_empty")),
         ("validator.env.in-scope", (Set.singleton $ HSIVar "GSDL.Validator" "gsvalidator_env_in_scope", HSVar "gsvalidator_env_in_scope")),
         ("whitespace", (Set.singleton $ HSIVar "GSDL.Syntax" "whitespace", HSVar "whitespace")),
         ("zip", (Set.singleton $ HSIVar "GSI.List" "gszip", HSVar "gszip")),
