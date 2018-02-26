e62da3c56d2b94e62cb39787807ad1db623a7830
jonathanccast <jonathanccast@fastmail.fm>
2018-02-26 03:27:35 +0000

    [hsgs] Add function (*)

--- src/hsgs2hs/HSGS/Syntax.hs
+++ src/hsgs2hs/HSGS/Syntax.hs
@@ -423,6 +423,7 @@ globalEnv = Env{
         ("value", \ env -> (:) <$> (ArgExpr <$> getPos <*> (EVar <$> getPos <*> lexeme identName)) <*> return [])
     ],
     leftops = Map.fromList [
+        ("*", ()),
         ("*>", ()),
         ("+", ()),
         ("<*", ()),
