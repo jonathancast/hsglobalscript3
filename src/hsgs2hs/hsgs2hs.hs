fdefa9c02a8e60b09dc008ca0b1d3088c16924d5
jonathanccast <jonathanccast@fastmail.fm>
2018-03-04 19:59:17 +0000

    [hsgs] Add view pat.view

--- src/hsgs2hs/hsgs2hs.hs
+++ src/hsgs2hs/hsgs2hs.hs
@@ -901,6 +901,7 @@ globalEnv = Env{
         ("order-of-being.pattern", (Set.singleton $ HSIVar "GSDL.AST" "gsorder_of_being_pattern_view", HSVar "gsorder_of_being_pattern_view")),
         ("ordered-map.empty", (Set.singleton $ HSIVar "GSI.Map" "gsordered_map_empty_view", HSVar "gsordered_map_empty_view")),
         ("pat.var", (Set.singleton $ HSIVar "GSDL.AST" "gspat_var_view", HSVar "gspat_var_view")),
+        ("pat.view", (Set.singleton $ HSIVar "GSDL.AST" "gspat_view_view", HSVar "gspat_view_view")),
         ("right", (Set.singleton $ HSIVar "GSI.Either" "gsright_view", HSVar "gsright_view")),
         ("true", (Set.singleton $ HSIVar "GSI.Bool" "gstrue_view", HSVar "gstrue_view")),
         ("type.app", (Set.singleton $ HSIVar "GSDL.AST" "gstype_app_view", HSVar "gstype_app_view")),
