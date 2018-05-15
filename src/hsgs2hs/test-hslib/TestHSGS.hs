e524037d2ed24196d032d05f73aab274c79b80f3
jonathanccast <jonathanccast@fastmail.fm>
2018-05-15 04:37:07 +0000

    [hsgs] Fix formatTestValue to handle string literals properly

--- src/hsgs2hs/test-hslib/TestHSGS.hs
+++ src/hsgs2hs/test-hslib/TestHSGS.hs
@@ -4,7 +4,7 @@ module TestHSGS (printTestExpr, printTestValue) where
 import qualified Data.Map as Map
 
 import GSI.Util (StackTrace(..), gshere, fmtPos)
-import GSI.Syn (GSVar, fmtVarAtom, fmtVarBindAtom)
+import GSI.Syn (GSVar, gsvar, fmtVarAtom, fmtVarBindAtom)
 import GSI.Error (fmtErrorShort)
 import GSI.Value (GSValue(..), GSBCO(..), GSExpr, gsthunk, gsvCode, bcoCode)
 import GSI.Eval (evalSync)
@@ -25,6 +25,7 @@ formatTestValue (GSClosure _ GSLambda{}) k = k $ ("<function>"++)
 formatTestValue (GSClosure _ bco) k = k $ ('<':) . fmtPos $gshere . ("unimpl: formatTestValue (GSClosure _ "++) . (bcoCode bco++) . (") next>"++)
 formatTestValue v@GSRecord{} k = formatTestValueAtom v k
 formatTestValue v@GSNatural{} k = formatTestValueAtom v k
+formatTestValue v@(GSConstr _ c [ GSRune ch, s ]) k | c == gsvar ":" = formatTestValueAtom v k
 formatTestValue (GSConstr _ v as) k = formatArgs as $ \ ds -> k (fmtVarAtom v . ds)
 formatTestValue v k = k $ ('<':) . fmtPos $gshere . ("unimpl: formatTestValue "++) . (gsvCode v++) . (" next>"++)
 
@@ -34,6 +35,8 @@ formatTestValueAtom (GSError err) k = k $ ('<':) . (fmtErrorShort err++) . ('>':
 formatTestValueAtom (GSThunk ts) k = do
     v <- evalSync [StackTrace $gshere []] ts
     formatTestValueAtom v k
+formatTestValueAtom (GSConstr _ c [ GSRune ch, s ]) k | c == gsvar ":" =
+    formatChar (GSRune ch) $ \ chds -> formatString s $ \ sds -> k (("qq{"++) . chds . sds . ('}':))
 formatTestValueAtom (GSConstr pos c []) k = k $ fmtVarAtom c
 formatTestValueAtom (GSRecord _ fs) k = case Map.null fs of
     True -> k $ ("〈〉"++)
@@ -52,3 +55,16 @@ formatFields :: [(GSVar, GSValue)] -> ((String -> String) -> IO a) -> IO a
 formatFields ((v, x):fs) k = formatTestValue x $ \ xds -> formatFields fs $ \ fsds ->
     k $ fmtVarBindAtom v . (" = "++) . xds . ("; "++) . fsds
 formatFields [] k = k id
+
+formatChar :: GSValue -> ((String -> String) -> IO a) -> IO a
+formatChar (GSRune ch) k = k $ (ch:)
+formatChar v k = k $ ('<':) . fmtPos $gshere . ("unimpl: formatChar "++) . (gsvCode v++) . (" next>"++)
+
+formatString :: GSValue -> ((String -> String) -> IO a) -> IO a
+formatString (GSThunk ts) k = do
+    v <- evalSync [StackTrace $gshere []] ts
+    formatString v k
+formatString (GSConstr _ c [ r0, s1 ]) k | c == gsvar ":" = formatChar r0 $ \ r0ds -> formatString s1 $ \ s1ds -> k $ r0ds . s1ds
+formatString (GSConstr _ c []) k | c == gsvar "nil" = k $ id
+formatString (GSConstr _ c as) k = k $ ('<':) . fmtPos $gshere . ("unimpl: formatString "++) . fmtVarAtom c . (" next>"++)
+formatString v k = k $ ('<':) . fmtPos $gshere . ("unimpl: formatString "++) . (gsvCode v++) . (" next>"++)
