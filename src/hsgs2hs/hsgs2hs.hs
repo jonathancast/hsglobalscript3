d1abf30af743535433913bcf642db6172dd2c2f0
jonathanccast <jonathanccast@fastmail.fm>
2018-05-24 20:02:03 +0000

    [hsgs] Add function annotate-qloitem

--- src/hsgs2hs/hsgs2hs.hs
+++ src/hsgs2hs/hsgs2hs.hs
@@ -720,6 +720,7 @@ globalEnv = Env{
         ("analyze-impM", (Set.singleton $ HSIVar "GSI.StdLib" "gsanalyzeImpM", HSVar "gsanalyzeImpM")),
         ("analyze-type-checker-M", (Set.singleton $ HSIVar "GSDL.TypeChecker" "gstype_checker_analyze", HSVar "gstype_checker_analyze")),
         ("annotate-document", (Set.singleton $ HSIVar "GSDL.Annotate" "gsannotate_document", HSVar "gsannotate_document")),
+        ("annotate-qloitem", (Set.singleton $ HSIVar "GSDL.Annotate" "gsannotate_qloitem", HSVar "gsannotate_qloitem")),
         ("annotator.env.category.get", (Set.singleton $ HSIVar "GSDL.Annotator" "gsannotator_env_category_get", HSVar "gsannotator_env_category_get")),
         ("annotator.env.category.insert", (Set.singleton $ HSIVar "GSDL.Annotator" "gsannotator_env_category_insert", HSVar "gsannotator_env_category_insert")),
         ("annotator.env.empty", (Set.singleton $ HSIVar "GSDL.Annotator" "gsannotator_env_empty", HSVar "gsannotator_env_empty")),
