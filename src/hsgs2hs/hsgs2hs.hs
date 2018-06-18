d0d7e07a997d496aef14d421295398dfd0f3a130
jonathanccast <jonathanccast@fastmail.fm>
2018-06-18 02:39:04 +0000

    [hsgs] Add function type.env.var-types

--- src/hsgs2hs/hsgs2hs.hs
+++ src/hsgs2hs/hsgs2hs.hs
@@ -941,6 +941,7 @@ globalEnv = Env{
         ("type.env.type.get", (Set.singleton $ HSIVar "GSDL.AST" "gstype_env_type_get", HSVar "gstype_env_type_get")),
         ("type.env.type.insert",  (Set.singleton $ HSIVar "GSDL.AST" "gstype_env_type_insert", HSVar "gstype_env_type_insert")),
         ("type.env.types",  (Set.singleton $ HSIVar "GSDL.AST" "gstype_env_types", HSVar "gstype_env_types")),
+        ("type.env.var-types",  (Set.singleton $ HSIVar "GSDL.AST" "gstype_env_var_types", HSVar "gstype_env_var_types")),
         ("type.env.vars",  (Set.singleton $ HSIVar "GSDL.AST" "gstype_env_vars", HSVar "gstype_env_vars")),
         ("type.env.view-type.get", (Set.singleton $ HSIVar "GSDL.AST" "gstype_env_view_type_get", HSVar "gstype_env_view_type_get")),
         ("type.env.view-type.insert", (Set.singleton $ HSIVar "GSDL.AST" "gstype_env_view_type_insert", HSVar "gstype_env_view_type_insert")),
