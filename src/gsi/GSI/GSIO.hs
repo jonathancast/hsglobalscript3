677ac821778aa64901ca4dbe9eaeb15f324087e4
jonathanccast <jonathanccast@fastmail.fm>
2018-07-18 19:21:11 +0000

    [hsgs] Fill out gsio.file.read

--- src/gsi/GSI/GSIO.hs
+++ src/gsi/GSI/GSIO.hs
@@ -1,13 +1,21 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TemplateHaskell, ImplicitParams, ScopedTypeVariables #-}
 module GSI.GSIO (gsio_monad, gsio_file_read) where
 
+import Prelude hiding (readFile, writeFile) -- Because Haskell is stupid and evil
+
 import qualified Data.Map as Map
+
+import Control.Exception (SomeException, try)
+
+import Data.Encoding.UTF8 (UTF8(..))
+import System.IO.Encoding (readFile)
 
 import GSI.Util (Pos, gshere)
 import GSI.Syn (gsvar)
 import GSI.ThreadType (Thread)
 import GSI.Value (GSValue(..), gslambda_value, gsimpprim, gsav, gsae)
 import API (apiImplementationFailure)
+import GSI.Functions (gslazystring, gsapiEvalString)
 import GSI.ByteCode (gsbcarg, gsbcapply, gsbcimpfor, gsbcimpbind, gsbcimpbody, gsbcimpunit)
 
 gsio_monad = GSRecord $gshere $ Map.fromList [
@@ -23,4 +31,10 @@ gsio_file_read = $gsimpprim gsioprim_file_read
 
 gsioprim_file_read :: Pos -> Thread -> GSValue -> IO GSValue
 gsioprim_file_read pos t fn = do
-    $apiImplementationFailure $ "gsioprim_file_read next"
+    fns <- gsapiEvalString $gshere fn
+    mbs <- try $ let ?enc = UTF8Strict in readFile fns
+    case mbs of
+        Left (e :: SomeException) -> $apiImplementationFailure $ "gsioprim_file_read " ++ show fns ++ " (readFile returned Left (" ++ show e ++ ")) next"
+        Right s -> do
+            sv <- $gslazystring s
+            return $ GSConstr $gshere (gsvar "right") [ sv ]
