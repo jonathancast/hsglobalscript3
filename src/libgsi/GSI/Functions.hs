21450353c59cf61ed322053eba3c63cbc944fb3a
jonathanccast <jonathanccast@fastmail.fm>
2018-12-19 04:19:17 +0000

    [hsgs] Add function gslazylist

--- src/libgsi/GSI/Functions.hs
+++ src/libgsi/GSI/Functions.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE TemplateHaskell, ScopedTypeVariables #-}
 {-# OPTIONS_GHC -fwarn-incomplete-patterns -fno-warn-overlapping-patterns #-}
-module GSI.Functions (gslist, gslist_w, gsstring, gsstring_w, gslazystring, gslazystring_w, gsnatural, gsnatural_w, gsapiEval, gsapiEvalPos, gsapiEvalList, gsapiEvalString, gsapiEvalNatural, gsapiEvalExternal, gsfmterrormsg) where
+module GSI.Functions (gslist, gslist_w, gsstring, gsstring_w, gslazylist, gslazylist_w, gslazystring, gslazystring_w, gsnatural, gsnatural_w, gsapiEval, gsapiEvalPos, gsapiEvalList, gsapiEvalString, gsapiEvalNatural, gsapiEvalExternal, gsfmterrormsg) where
 
 import Control.Exception (Exception(..), throwIO, try)
 
@@ -20,6 +20,8 @@ gslist = varE 'gslist_w `appE` gshere
 gslist_w :: Pos -> [GSValue] -> GSValue
 gslist_w pos [] = GSConstr pos (gsvar "nil") []
 gslist_w pos (x:xn) = GSConstr pos (gsvar ":") [ x, gslist_w pos xn ]
+
+gslazylist = varE 'gslazylist_w `appE` gshere
 
 gslazylist_w :: Pos -> [GSValue] -> IO GSValue
 gslazylist_w pos xn = gsthunk_w pos $ GSExpr $ \ cs sk -> case xn of
