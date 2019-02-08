7ba6a9f1462e7a40a245f21efe3705852f9aaf02
jonathanccast <jonathanccast@fastmail.fm>
2019-02-08 22:13:58 +0000

    [hsgs] Add function hsimport.fmt

--- src/hsgs2hs/hsgs2hs.hs
+++ src/hsgs2hs/hsgs2hs.hs
@@ -884,6 +884,7 @@ globalEnv = Env{
         ("hsgs.compiler.env.empty", "GSDL.HSGS.Compiler", "gshsgs_compiler_env_empty"),
         ("hsgs.compiler.env.var.get", "GSDL.HSGS.Compiler", "gshsgs_compiler_env_var_get"),
         ("hsgs.compiler.env.var.insert", "GSDL.HSGS.Compiler", "gshsgs_compiler_env_var_insert"),
+        ("hsimport.fmt", "GSDL.HSGS.AST", "gshsimport_fmt"),
         ("ident", "GSDL.Syntax", "ident"),
         ("impanalyzeM", "GSI.StdLib", "gsanalyzeImpM"),
         ("impfor", "GSI.StdLib", "gsimpfor"),
