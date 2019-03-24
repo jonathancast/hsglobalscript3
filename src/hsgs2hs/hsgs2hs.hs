ffbe810ea2136d3dbbd5a87258b630033273ad3e
jonathanccast <jonathanccast@fastmail.fm>
2019-03-24 22:50:46 +0000

    [hsgs-list] Add function list.∃

--- src/hsgs2hs/hsgs2hs.hs
+++ src/hsgs2hs/hsgs2hs.hs
@@ -890,6 +890,7 @@ globalEnv = Env{
         ("left", "GSI.Either", "gsleft"),
         ("lexeme", "GSDL.Syntax", "lexeme"),
         ("list.<=>", "GSI.List", "gslist_compare"),
+        ("list.∃", "GSI.List", "gslist_exists"),
         ("list.∈", "GSI.List", "gslist_member"),
         ("list.∉", "GSI.List", "gslist_not_member"),
         ("list.≡", "GSI.List", "gslist_eq"),
