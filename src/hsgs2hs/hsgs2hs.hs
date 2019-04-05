f446339c46e0a2e1a0007392aee136c7fe6613f0
jonathanccast <jonathanccast@fastmail.fm>
2019-04-05 19:56:10 +0000

    [hsgs] Add function parser.env.generator.modify

--- src/hsgs2hs/hsgs2hs.hs
+++ src/hsgs2hs/hsgs2hs.hs
@@ -962,6 +962,7 @@ globalEnv = Env{
         ("parser.env.else.insert", "GSDL.Parser", "gsdl_parser_env_else_insert"),
         ("parser.env.empty", "GSDL.Parser", "gsdl_parser_env_empty"),
         ("parser.env.generator.get", "GSDL.Parser", "gsdl_parser_env_generator_get"),
+        ("parser.env.generator.modify", "GSDL.Parser", "gsdl_parser_env_generator_modify"),
         ("parser.env.generator.set", "GSDL.Parser", "gsdl_parser_env_generator_set"),
         ("parser.env.missing-body.get", "GSDL.Parser", "gsdl_parser_env_missing_body_get"),
         ("parser.env.missing-body.insert", "GSDL.Parser", "gsdl_parser_env_missing_body_insert"),
