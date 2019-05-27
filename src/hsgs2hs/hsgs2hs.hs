2a1171f6a7fcafae04cf00604684fa1596618384
jonathanccast <jonathanccast@fastmail.fm>
2019-05-27 19:45:07 +0000

    [hsgs] Add function hsgs.compiler.env.generator.get

--- src/hsgs2hs/hsgs2hs.hs
+++ src/hsgs2hs/hsgs2hs.hs
@@ -872,6 +872,7 @@ globalEnv = Env{
         ("hsexpr.var", "GSDL.HSGS.AST", "gshsexpr_var"),
         ("hsgs.compiler.env.empty", "GSDL.HSGS.Compiler", "gshsgs_compiler_env_empty"),
         ("hsgs.compiler.env.gen-sym", "GSDL.HSGS.Compiler", "gshsgs_compiler_env_gen_sym"),
+        ("hsgs.compiler.env.generator.get", "GSDL.HSGS.Compiler", "gshsgs_compiler_env_generator_get"),
         ("hsgs.compiler.env.var.get", "GSDL.HSGS.Compiler", "gshsgs_compiler_env_var_get"),
         ("hsgs.compiler.env.var.insert", "GSDL.HSGS.Compiler", "gshsgs_compiler_env_var_insert"),
         ("hsgs.compiler.env.view.get", "GSDL.HSGS.Compiler", "gshsgs_compiler_env_view_get"),
