8888fd588f3af1bc49565cdef62c00b097fd68b0
Jonathan Cast <jonathanccast@fastmail.fm>
2019-12-30 03:48:34 +0000

    [hsgs] Add function hsoutput.applyivarpos

--- src/hsgs2hs/hsgs2hs.hs
+++ src/hsgs2hs/hsgs2hs.hs
@@ -905,6 +905,7 @@ globalEnv = Env{
         ("hsoutput.app", "GSDL.HSGS.AST", "gshsoutput_app"),
         ("hsoutput.apply", "GSDL.HSGS.AST", "gshsoutput_apply"),
         ("hsoutput.applyivar", "GSDL.HSGS.AST", "gshsoutput_applyivar"),
+        ("hsoutput.applyivarpos", "GSDL.HSGS.AST", "gshsoutput_applyivarpos"),
         ("hsoutput.arg-expr", "GSDL.HSGS.AST", "gshsoutput_arg_expr"),
         ("hsoutput.arg-var", "GSDL.HSGS.AST", "gshsoutput_arg_var"),
         ("hsoutput.iconstr", "GSDL.HSGS.AST", "gshsoutput_iconstr"),
