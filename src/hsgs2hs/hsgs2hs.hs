65ab4c7b91f2f76e620c8776b22fd29a0558bec8
Jonathan Cast <jonathanccast@fastmail.fm>
2020-04-27 00:45:12 +0000

    [type] Add function type.env.arg-var-type.insert

--- src/hsgs2hs/hsgs2hs.hs
+++ src/hsgs2hs/hsgs2hs.hs
@@ -1145,6 +1145,7 @@ globalEnv = Env{
         ("type.consts", "GSDL.AST", "gstype_consts"),
         ("type.empty-signature", "GSDL.AST", "gstype_empty_signature"),
         ("type.env.arg-var-type.get", "GSDL.AST", "gstype_env_arg_var_type_get"),
+        ("type.env.arg-var-type.insert", "GSDL.AST", "gstype_env_arg_var_type_insert"),
         ("type.env.empty",  "GSDL.AST", "gstype_env_empty"),
         ("type.env.filter",  "GSDL.AST", "gstype_env_filter"),
         ("type.env.generator-type.get", "GSDL.AST", "gstype_env_generator_type_get"),
