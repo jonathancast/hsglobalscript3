4e60aa6976fe87f8eeac2a20841cd59426912e9d
Jonathan Cast <jonathanccast@fastmail.fm>
2021-01-03 22:31:55 +0000

    [rts] Add new channel implementation
    * Allows bit-bucket channels with automated GC
    * Allows tryReadChannel and an Event to wait for a channel to be readable

--- src/libgsi/GSI/RTS.hs
+++ src/libgsi/GSI/RTS.hs
@@ -1,13 +1,16 @@
-module GSI.RTS (Event, newEvent, await, awaitAny, wakeup) where
+module GSI.RTS (Event, newEvent, await, awaitAny, wakeup, OPort, newChannel, bitBucketOPort, iportReadable, tryReadIPort) where
 
 import Control.Monad (forM_)
 
-import Control.Concurrent (MVar, newMVar, newEmptyMVar, modifyMVar, modifyMVar_, takeMVar, tryPutMVar)
+import Control.Concurrent (MVar, newMVar, newEmptyMVar, modifyMVar, modifyMVar_, takeMVar, readMVar, putMVar, tryPutMVar)
 
 newtype Event = Event (MVar (Maybe [MVar ()]))
 
 newEvent :: IO Event
 newEvent = Event <$> newMVar (Just [])
+
+occurredEvent :: IO Event
+occurredEvent = Event <$> newMVar Nothing
 
 await :: Event -> IO ()
 await (Event mv) = do
@@ -32,3 +35,78 @@ wakeup (Event mv) = do
         Nothing -> return (Nothing, [])
         Just mvs -> return (Nothing, mvs)
     mapM_ (`tryPutMVar` ()) mvs
+
+newtype ChannelEntry a = ChannelEntry { unChannelEntry :: MVar (Either Event (a, ChannelEntry a)) }
+
+newtype IPort a = IPort { unIPort :: MVar (ChannelEntry a) }
+
+newtype OPort a = OPort { unOPort :: MVar (ChannelEntry a) }
+
+newChannel :: IO (IPort a, OPort a)
+newChannel = do
+    ev <- newEvent
+    ce <- ChannelEntry <$> newMVar (Left ev)
+    ip <- IPort <$> newMVar ce
+    op <- OPort <$> newMVar ce
+    return (ip, op)
+
+bitBucketOPort :: IO (OPort a)
+bitBucketOPort = do
+    ev <- newEvent
+    ce <- ChannelEntry <$> newMVar (Left ev)
+    op <- OPort <$> newMVar ce
+    return op
+
+iportReadable :: IPort a -> IO Event
+iportReadable ip = do
+    ce <- readMVar $ unIPort ip
+    e <- readMVar $ unChannelEntry ce
+    case e of
+        Left ev -> return ev
+        Right _ -> occurredEvent
+
+readIPort :: IPort a -> IO a
+readIPort ip = do
+    ce <- takeMVar $ unIPort ip
+    w ce
+  where
+    w ce = do
+        e <- takeMVar $ unChannelEntry ce
+        case e of
+            Left ev -> do
+                unChannelEntry ce `putMVar` e
+                await ev
+                w ce
+            Right (x, ce1) -> do
+                unIPort ip `putMVar` ce1
+                return x
+
+tryReadIPort :: IPort a -> IO (Maybe a)
+tryReadIPort ip = do
+    ce <- takeMVar $ unIPort ip
+    e <- readMVar $ unChannelEntry ce
+    case e of
+        Left ev -> do
+            unIPort ip `putMVar` ce
+            return Nothing
+        Right (x, ce1) -> do
+            unIPort ip `putMVar` ce1
+            return (Just x)
+
+writeOPort :: OPort a -> a -> IO ()
+writeOPort op x = do
+    ce <- takeMVar $ unOPort op
+    w ce
+  where
+    w ce = do
+        e <- takeMVar $ unChannelEntry ce
+        case e of
+            Left ev -> do
+                ev1 <- newEvent
+                ce1 <- ChannelEntry <$> newMVar (Left ev1)
+                unChannelEntry ce `putMVar` Right (x, ce1)
+                unOPort op `putMVar` ce1
+                wakeup ev
+            Right (_, ce1) -> do
+                unChannelEntry ce `putMVar` e
+                w ce1
