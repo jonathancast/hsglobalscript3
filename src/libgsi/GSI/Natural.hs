72d3c4daaf67935c32b2fe99c126900fe612b4de
Jonathan Cast <jonathanccast@fastmail.fm>
2021-01-24 23:44:16 +0000

    [natural] Factor out gsnatural_rel

--- src/libgsi/GSI/Natural.hs
+++ src/libgsi/GSI/Natural.hs
@@ -41,20 +41,16 @@ gsnatural_op f = $gslambda_value $ \ n0 -> $gsbcarg $ \ n1 -> $gsbcforce ($gsav 
     (GSNatural n0hs, GSNatural n1hs) -> $gsbcenter $ GSNatural $ f n0hs n1hs
     _ -> $gsbcimplementationfailure $ "gsnatural_op " ++ gsvCode n0_0 ++ ' ' : gsvCode n1_0 ++ " next"
 
-gsnatural_eq = $gslambda_value $ \ n0 -> $gsbcarg $ \ n1 -> $gsbcforce ($gsav n0) $ \ n0_0 -> $gsbcforce ($gsav n1) $ \ n1_0 -> case (n0_0, n1_0) of
-    (GSNatural n0hs, GSNatural n1hs) ->
-        if n0hs == n1hs then $gsbcconstr (gsvar "true") [] else $gsbcconstr (gsvar "false") []
-    _ -> $gsbcimplementationfailure $ "gsnatural_eq " ++ gsvCode n0_0 ++ ' ' : gsvCode n1_0 ++ " next"
+gsnatural_eq = gsnatural_rel (==)
 
-gsnatural_neq = $gslambda_value $ \ n0 -> $gsbcarg $ \ n1 -> $gsbcforce ($gsav n0) $ \ n0_0 -> $gsbcforce ($gsav n1) $ \ n1_0 -> case (n0_0, n1_0) of
-    (GSNatural n0hs, GSNatural n1hs) ->
-        if n0hs /= n1hs then $gsbcconstr (gsvar "true") [] else $gsbcconstr (gsvar "false") []
-    _ -> $gsbcimplementationfailure $ "gsnatural_neq " ++ gsvCode n0_0 ++ ' ' : gsvCode n1_0 ++ " next"
+gsnatural_neq = gsnatural_rel (/=)
 
-gsnatural_gt = $gslambda_value $ \ n0 -> $gsbcarg $ \ n1 -> $gsbcforce ($gsav n0) $ \ n0_0 -> $gsbcforce ($gsav n1) $ \ n1_0 -> case (n0_0, n1_0) of
+gsnatural_gt = gsnatural_rel (>)
+
+gsnatural_rel rel = $gslambda_value $ \ n0 -> $gsbcarg $ \ n1 -> $gsbcforce ($gsav n0) $ \ n0_0 -> $gsbcforce ($gsav n1) $ \ n1_0 -> case (n0_0, n1_0) of
     (GSNatural n0hs, GSNatural n1hs) ->
-        if n0hs > n1hs then $gsbcconstr (gsvar "true") [] else $gsbcconstr (gsvar "false") []
-    _ -> $gsbcimplementationfailure $ "gsnatural_eq " ++ gsvCode n0_0 ++ ' ' : gsvCode n1_0 ++ " next"
+        if rel n0hs n1hs then $gsbcconstr (gsvar "true") [] else $gsbcconstr (gsvar "false") []
+    _ -> $gsbcimplementationfailure $ "gsnatural_rel " ++ gsvCode n0_0 ++ ' ' : gsvCode n1_0 ++ " next"
 
 gsnatural_cmp = $gslambda_value $ \ n0 -> $gsbcarg $ \ n1 -> $gsbcforce ($gsav n0) $ \ n0_0 -> $gsbcforce ($gsav n1) $ \ n1_0 -> case (n0_0, n1_0) of
     (GSNatural n0hs, GSNatural n1hs) -> case n0hs `compare` n1hs of
