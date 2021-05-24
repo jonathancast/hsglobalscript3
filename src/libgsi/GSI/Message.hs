383ef526ee77a38bbaf5b3ab512f12e9f1205274
Jonathan Cast <jonathanccast@fastmail.fm>
2021-05-24 03:18:59 +0000

    [logging] Add info messages

--- src/libgsi/GSI/Message.hs
+++ src/libgsi/GSI/Message.hs
@@ -1,9 +1,20 @@
 {-# OPTIONS_GHC -fwarn-incomplete-patterns -fno-warn-overlapping-patterns #-}
-module GSI.Message (Message(..), msgCode) where
+module GSI.Message (Message(..), GSMessageClass(..), mkMessage, msgCode, msgClassCode) where
 
 import GSI.Util (StackTrace)
+import {-# SOURCE #-} GSI.Value (GSValue)
 
 data Message
   = MsgProfile StackTrace
+  | MsgInfo StackTrace GSValue
+
+data GSMessageClass
+  = GSInfo
+
+mkMessage :: StackTrace -> GSMessageClass -> GSValue -> Message
+mkMessage st GSInfo v = MsgInfo st v
 
 msgCode MsgProfile{} = "MsgProfile"
+msgCode MsgInfo{} = "MsgInfo"
+
+msgClassCode GSInfo = "GSInfo"
